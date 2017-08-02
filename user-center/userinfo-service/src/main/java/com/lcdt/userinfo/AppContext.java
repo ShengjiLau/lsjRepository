0dfa0d013dde11daf68761e94507f4f6c7859ef1
ss <18754988272@163.com>
2017-08-02 14:53:53 +0800

    修改启动线程，加入spring boot build plugin

--- user-center/userinfo-service/src/main/java/com/lcdt/userinfo/AppContext.java
+++ user-center/userinfo-service/src/main/java/com/lcdt/userinfo/AppContext.java
@@ -1,32 +1,33 @@
 package com.lcdt.userinfo;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.util.concurrent.CountDownLatch;
 
 /**
  * Created by ss on 2017/7/31.
  */
 @SpringBootApplication
 @ImportResource("dubbo-user-provider.xml")
-public class AppContext implements EnvironmentPostProcessor, CommandLineRunner {
+public class AppContext {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppContext.class, args);
-		while (true) {
-			//block main thread ，otherwise spring boot will shutdown
+
+		ConfigurableApplicationContext run = new SpringApplicationBuilder()
+				.sources(AppContext.class)
+				.web(false)
+				.run(args);
+
+		//block main thread
+		CountDownLatch countDownLatch = new CountDownLatch(1);
+		try {
+			countDownLatch.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
-	}
 
-	@Override
-	public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
-		application.setWebEnvironment(false);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
 	}
 }
