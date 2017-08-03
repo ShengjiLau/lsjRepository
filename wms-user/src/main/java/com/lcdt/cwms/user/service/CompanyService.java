c37aa4eb4450f3b10a7a7774a9bb74526a72fb05
ss <18754988272@163.com>
2017-08-03 13:40:20 +0800

    新增 company dao

---
+++ wms-user/src/main/java/com/lcdt/cwms/user/service/CompanyService.java
@@ -0,0 +1,10 @@
+package com.lcdt.cwms.user.service;
+
+/**
+ * Created by ss on 2017/8/3.
+ */
+public interface CompanyService {
+
+
+
+}
---
+++ wms-user/src/test/java/com/lcdt/cwms/user/DepartDesign.java
@@ -0,0 +1,22 @@
+package com.lcdt.cwms.user;
+
+import org.junit.Test;
+
+/**
+ * Created by ss on 2017/8/2.
+ */
+public class DepartDesign {
+
+
+	@Test
+	public void testDepart(){
+
+
+
+
+	}
+
+
+
+
+}
