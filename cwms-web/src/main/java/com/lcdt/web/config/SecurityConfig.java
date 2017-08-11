2d28dc5504bf5559f75340a4bcaa27ad58f3f0a4
ss <18754988272@163.com>
2017-08-11 16:57:48 +0800

    处理登陆错误信息

--- cwms-web/src/main/java/com/lcdt/web/config/SecurityConfig.java
+++ cwms-web/src/main/java/com/lcdt/web/config/SecurityConfig.java
@@ -2,17 +2,14 @@ package com.lcdt.web.config;
 
 import com.lcdt.web.auth.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -46,6 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 					public String encode(CharSequence rawPassword) {
 						return RegisterUtils.md5Encrypt(rawPassword.toString());
 					}
+
 					@Override
 					public boolean matches(CharSequence rawPassword, String encodedPassword) {
 						return encode(rawPassword).toUpperCase().equals(encodedPassword.toUpperCase());
@@ -75,7 +73,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
 	@Bean
-	public AjaxLoginFailureHandler loginFailureHandler(){
+	public AjaxLoginFailureHandler loginFailureHandler() {
 		return new AjaxLoginFailureHandler();
 	}
 
