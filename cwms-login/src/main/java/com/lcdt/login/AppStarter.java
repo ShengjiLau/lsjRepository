e6f7f73b8cea4fb5a7493c4e5bd0b52f2ee3c1ac
mawer111 <18754988272@163.com>
2017-08-18 10:04:30 +0800

    配置dubbo

--- cwms-login/src/main/java/com/lcdt/login/AppStarter.java
+++ cwms-login/src/main/java/com/lcdt/login/AppStarter.java
@@ -1,6 +1,7 @@
 package com.lcdt.login;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 /**
  * Created by ss on 2017/8/17.
@@ -8,6 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AppStarter {
 
-
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(AppStarter.class)
+				.run(args);
+	}
 
 }
