741b1ef2b21e147d61d91973093c4712295f1245
mawer111 <18754988272@163.com>
2017-09-21 17:39:02 +0800

    几个脚本

---
+++ scripts/addVirtualHost.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#设置虚拟host
+SSO_SERVER="login.datuodui.com"
+TEST_SERVER="test.datuodui.com"
+DEMO_SERVER="demo.datuodui.com"
+
+HOST_PATH="/etc/hosts";
+LOCAL_IP="127.0.0.1"
+
+
+
+echo -e "${LOCAL_IP} \t ${SSO_SERVER} " >> /etc/hosts
+echo -e "${LOCAL_IP} \t ${TEST_SERVER} " >> /etc/hosts
+echo -e "${LOCAL_IP} \t ${DEMO_SERVER} " >> /etc/hosts
+
