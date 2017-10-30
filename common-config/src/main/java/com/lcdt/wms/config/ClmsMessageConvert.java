ed27246090239d4e3f313ab78d1ef6663f1e06b8
mawer111 <18754988272@163.com>
2017-10-30 12:00:03 +0800

    新增 ClmsMessafeConvert

---
+++ common-config/src/main/java/com/lcdt/wms/config/ClmsMessageConvert.java
@@ -0,0 +1,27 @@
+package com.lcdt.wms.config;
+
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+
+import java.io.IOException;
+
+/**
+ * Created by ss on 2017/10/30.
+ */
+public class ClmsMessageConvert extends FastJsonHttpMessageConverter {
+
+	public JSONObject successObject = new JSONObject();
+
+	@Override
+	protected void writeInternal(Object obj, HttpOutputMessage outputMessage) throws IOException, HttpMessageNotWritableException {
+
+
+		successObject.put("data", obj);
+		successObject.put("message", "请求成功");
+		successObject.put("code", 0);
+
+		super.writeInternal(successObject, outputMessage);
+	}
+}
