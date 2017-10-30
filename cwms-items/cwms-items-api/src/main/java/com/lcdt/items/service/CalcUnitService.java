0030ffe1f556ea7d40e0c25b16154e3e1534f512
lyqishan <lyqishan@126.com>
2017-10-30 11:14:52 +0800

    login fix bugs

---
+++ cwms-items/cwms-items-api/src/main/java/com/lcdt/items/service/CalcUnitService.java
@@ -0,0 +1,8 @@
+package com.lcdt.items.service;
+
+/**
+ * Created by lyqishan on 2017/10/30
+ */
+
+public interface CalcUnitService {
+}
---
+++ cwms-items/cwms-items-service/src/main/java/com/lcdt/items/service/impl/ItemClassifyServiceImpl.java
@@ -0,0 +1,10 @@
+package com.lcdt.items.service.impl;
+
+import com.lcdt.items.service.ItemClassifyService;
+
+/**
+ * Created by lyqishan on 2017/10/30
+ */
+
+public class ItemClassifyServiceImpl implements ItemClassifyService{
+}
