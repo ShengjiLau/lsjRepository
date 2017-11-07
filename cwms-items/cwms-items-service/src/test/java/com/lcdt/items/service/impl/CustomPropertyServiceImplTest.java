ad5cde6f8a18d204805d1d7ffb91175bc6c2f8c2
liuhu <liuhu@lichendt.com>
2017-11-07 14:30:10 +0800

    处理sql语句错误

---
+++ cwms-items/cwms-items-service/src/test/java/com/lcdt/items/service/impl/CustomPropertyServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.lcdt.items.service.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @AUTHOR liuh
+ * @DATE 2017-11-07
+ */
+public class CustomPropertyServiceImplTest {
+    @Test
+    public void customPropertyList() throws Exception {
+    }
+
+    @Test
+    public void updateByCustomId() throws Exception {
+    }
+
+    @Test
+    public void delByCustomId() throws Exception {
+    }
+
+}
