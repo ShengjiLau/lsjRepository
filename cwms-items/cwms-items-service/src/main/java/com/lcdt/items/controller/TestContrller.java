4678a99ec7d11e0ff06ea2aff86a185e41013834
lyqishan <lyqishan@126.com>
2017-11-09 11:56:14 +0800

    更新

--- cwms-items/cwms-items-service/src/main/java/com/lcdt/items/controller/TestContrller.java
+++ cwms-items/cwms-items-service/src/main/java/com/lcdt/items/controller/TestContrller.java
@@ -2,19 +2,20 @@ package com.lcdt.items.controller;
 
 import com.lcdt.items.service.ItemClassifyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by lyqishan on 2017/11/8
  */
-@Controller
+@RestController
 public class TestContrller {
     @Autowired
     private ItemClassifyService itemClassifyService;
 
     @RequestMapping("/hello")
-    public void test(){
-        itemClassifyService.deleteItemsClassifyAndchildren(1L);
+    public String test() {
+        int i = itemClassifyService.deleteItemsClassifyAndChildren(45L);
+        return "删除行数" + i;
     }
 }
