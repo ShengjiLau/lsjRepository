7faa40c11416e265524b610c1cc998cd83061d17
liuhu <liuhu@lichendt.com>
2017-11-13 18:59:44 +0800

    商品列表service方法完成

--- cwms-items/cwms-items-api/src/main/java/com/lcdt/items/model/ItemsInfoDao.java
+++ cwms-items/cwms-items-api/src/main/java/com/lcdt/items/model/ItemsInfoDao.java
@@ -5,13 +5,13 @@ package com.lcdt.items.model;
  * @DATE 2017-11-13
  */
 public class ItemsInfoDao extends ItemsInfo {
-    private String classifys;   //商品分类id串，英文逗号分隔
+    private String classifyIds;   //商品分类id串，英文逗号分隔
 
-    public String getClassifys() {
-        return classifys;
+    public String getClassifyIds() {
+        return classifyIds;
     }
 
-    public void setClassifys(String classifys) {
-        this.classifys = classifys;
+    public void setClassifyIds(String classifyIds) {
+        this.classifyIds = classifyIds;
     }
 }
