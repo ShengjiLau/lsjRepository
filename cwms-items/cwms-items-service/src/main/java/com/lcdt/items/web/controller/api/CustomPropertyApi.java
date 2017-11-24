eed9449fba5ee91f929c2c4085f1963faeab2f23
liuhu <liuhu@lichendt.com>
2017-11-24 11:34:02 +0800

    controller异常处理类

--- cwms-items/cwms-items-service/src/main/java/com/lcdt/items/web/controller/api/CustomPropertyApi.java
+++ cwms-items/cwms-items-service/src/main/java/com/lcdt/items/web/controller/api/CustomPropertyApi.java
@@ -1,6 +1,8 @@
-package com.lcdt.items.controller;
+package com.lcdt.items.web.controller.api;
 
-import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.fastjson.JSONObject;
+import com.lcdt.converter.ArrayListResponseWrapper;
+import com.lcdt.converter.ResponseData;
 import com.lcdt.items.model.CustomProperty;
 import com.lcdt.items.service.CustomPropertyService;
 import io.swagger.annotations.Api;
@@ -12,7 +14,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.naming.event.ObjectChangeListener;
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.List;
@@ -25,9 +26,9 @@ import java.util.Map;
 @Api("商品自定义属性api")
 @RestController
 @RequestMapping("/items/customproperty")
-public class CustomPropertyController {
+public class CustomPropertyApi {
 
-    Logger logger = LoggerFactory.getLogger(CustomPropertyController.class);
+    Logger logger = LoggerFactory.getLogger(CustomPropertyApi.class);
 
     //@Reference
     @Autowired
@@ -35,32 +36,28 @@ public class CustomPropertyController {
 
     @ApiOperation(value = "获取企业下所有的自定义属性", notes = "获取企业下所有的自定义属性")
     @GetMapping("/list")
-    public ResponseEntity<Map<String,Object>> getCustomProperty(HttpSession httpSession) {
+    public List<CustomProperty> getCustomProperty(HttpSession httpSession) {
         logger.info("customPropertyService------------------", customPropertyService.getClass().getMethods().toString());
         Long companyId = 8L;  //TODO 从session获取companyId
         List<CustomProperty> customPropertyList = customPropertyService.customPropertyList(companyId);
-        Map<String,Object> map = new HashMap<String, Object>();
-        map.put("status",1);
-        map.put("data",customPropertyList);
-        return new ResponseEntity<Map<String,Object>>(map, HttpStatus.OK);
+        ArrayListResponseWrapper arrayListResponseWrapper = new ArrayListResponseWrapper(customPropertyList);
+        if(1==1){
+            throw new RuntimeException("异常");
+        }
+        return arrayListResponseWrapper;
     }
 
     @ApiOperation(value = "更新自定义属性", notes = "更新对应企业下的自定义属性")
     @PostMapping("/update")
-    public ResponseEntity<Map<String,Object>> updateCoustomProperty(CustomProperty customProperty, HttpSession httpSession) {
+    public JSONObject updateCoustomProperty(CustomProperty customProperty, HttpSession httpSession) {
         Map<String,Object> map = new HashMap<String, Object>();
-        try {
-            Long companyId = 8L;  //TODO 从session获取companyId
-            customProperty.setCompanyId(companyId);
-            customPropertyService.updateByCustomId(customProperty);
-            map.put("status",1);
-            map.put("message","更新成功");
-        } catch (Exception e) {
-            e.printStackTrace();
-            map.put("status",0);
-            map.put("message","更新失败");
-        }
-        return new ResponseEntity<Map<String,Object>>(map, HttpStatus.OK);
+        Long companyId = 8L;  //TODO 从session获取companyId
+        customProperty.setCompanyId(companyId);
+        customPropertyService.updateByCustomId(customProperty);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("code","0");
+        jsonObject.put("message","更新成功");
+        return jsonObject;
     }
 
     @ApiOperation(value = "删除自定义属性", notes = "根据主键删除对应自定义属性")
@@ -69,11 +66,11 @@ public class CustomPropertyController {
         Map<String,Object> map = new HashMap<String, Object>();
         try {
             customPropertyService.delByCustomId(customId);
-            map.put("status",1);
+            map.put("code",1);
             map.put("message","删除成功");
         } catch (Exception e) {
             e.printStackTrace();
-            map.put("status",0);
+            map.put("code",0);
             map.put("message","删除失败");
         }
         return new ResponseEntity<Map<String,Object>>(map, HttpStatus.OK);
@@ -87,11 +84,11 @@ public class CustomPropertyController {
             Long companyId = 8L;  //TODO 从session获取companyId
             customProperty.setCompanyId(companyId);
             customPropertyService.insertCustomProperty(customProperty);
-            map.put("status",1);
+            map.put("code",1);
             map.put("message","新增成功");
         } catch (Exception e) {
             e.printStackTrace();
-            map.put("status",0);
+            map.put("code",0);
             map.put("message","新增失败");
         }
         return new ResponseEntity<Map<String,Object>>(map, HttpStatus.OK);
