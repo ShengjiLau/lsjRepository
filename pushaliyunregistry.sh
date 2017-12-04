510d81fb58e6e5355465ac6db50543eca7091457
mawer111 <18754988272@163.com>
2017-12-04 09:50:32 +0800

    新增推送到阿里云 docker registry logback 配置

--- pushaliyunregistry.sh
+++ pushaliyunregistry.sh
@@ -1,25 +1,42 @@
 #!/usr/bin/env bash
+#阿里docker registry地址
+registry_url=registry.cn-hangzhou.aliyuncs.com/lcdt-clms/
 
-pushaliyun(){
-
+docker_login(){
 docker login --username=hi35700248@aliyun.com -p A1111777  registry.cn-hangzhou.aliyuncs.com
-cd user-center/userinfo-service/
-docker build -t registry.cn-hangzhou.aliyuncs.com/lcdt-clms/clms/demo-user-service .
-imageid=$(docker images | grep registry.cn-hangzhou.aliyuncs.com/lcdt-clms/clms/demo-user-service | awk '{print $3}')
-docker tag $imageid registry.cn-hangzhou.aliyuncs.com/lcdt-clms/clms/demo-user-service
-docker push registry.cn-hangzhou.aliyuncs.com/lcdt-clms/clms/demo-user-service
 }
 
+
+push_to_aliregistry(){
+imagename=$1
+imagetag=$2
+buildpath=$3
+fullimagename=$registry_url$1
+imagenamewithtag=$fullimagename:$2
+echo $imagenamewithtag
+docker build -t $fullimagename $3
+imageid=$(docker images | grep $fullimagename | awk '{print $3}')
+docker tag $imageid $imagenamewithtag
+docker push $imagenamewithtag
+}
+
+maven_build(){
 mvn clean install package -Dmaven.test.skip=true
 STATUS=$?
 
 if [[ $STATUS -eq 0 ]] ; then
 echo "编译成功"
 say "编译成功"
-pushaliyun
+push_to_aliregistry
 else
 echo "编译失败"
 say "编译失败"
 fi
+}
+docker_login
+read -p "请输入镜像名称: " imagename
+read -p "请输入镜像tag: " imagetag
+read -p "请输入构建路径: " imagepath
+push_to_aliregistry $imagename $imagetag $imagepath
 
 
