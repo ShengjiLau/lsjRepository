4595bc9373b842739cbc3b28521788b626a42b23
yangbinq <ybq_9861@163.com>
2017-12-05 14:57:44 +0800

    'ok'

--- clms-customer/clms-customer-service/src/main/java/com/lcdt/customer/CustomerServiceApp.java
+++ clms-customer/clms-customer-service/src/main/java/com/lcdt/customer/CustomerServiceApp.java
@@ -1,6 +1,7 @@
 package com.lcdt.customer;
 
 import com.lcdt.clms.security.annontion.EnableClmsSecurity;
+import com.lcdt.converter.ClmsResponseConvertConfig;
 import com.lcdt.wms.config.DubboConfig;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -14,7 +15,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  */
 @SpringBootApplication
 @EnableTransactionManagement
-@Import({com.lcdt.swagger.SwaggerConfig.class, DubboConfig.class})
+@Import({com.lcdt.swagger.SwaggerConfig.class, DubboConfig.class, ClmsResponseConvertConfig.class})
 @EnableClmsSecurity
 public class CustomerServiceApp {
 	public static void main(String[] args) {
