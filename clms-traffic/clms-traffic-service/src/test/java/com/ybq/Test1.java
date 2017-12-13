f6bd0b0b31ae564c7c9205d24dd60d37a07c1197
yangbinq <ybq_9861@163.com>
2017-12-13 11:22:19 +0800

    '测试接口'

--- clms-traffic/clms-traffic-service/src/test/java/com/ybq/Test1.java
+++ clms-traffic/clms-traffic-service/src/test/java/com/ybq/Test1.java
@@ -1,17 +1,35 @@
 package com.ybq;
 
+import com.lcdt.traffic.TrafficServiceApp;
+import com.lcdt.traffic.dao.WaybillPlanMapper;
+import com.lcdt.userinfo.dto.RegisterDto;
+import com.lcdt.userinfo.exception.PhoneHasRegisterException;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * @AUTHOR liuh
  * @DATE 2017-11-16
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes=Test1.class)
+@SpringBootTest(classes=TrafficServiceApp.class)
 public class Test1 {
 
 
+    @Autowired
+    private WaybillPlanMapper waybillPlanMapper;
+
+    @Test
+    @Rollback
+    public void testRegister() {
+        System.out.println(1111);
+        System.out.println(waybillPlanMapper);
+    }
 
 }
