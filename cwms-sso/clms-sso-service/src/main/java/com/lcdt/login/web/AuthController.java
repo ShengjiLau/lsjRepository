14631e37d42ed835eed6b48a921ce55a4bcfd0ec
mawei <mawei@lichendt.com>
2017-12-27 15:27:32 +0800

    Merge branches 'dev_pay_service' and 'userdev' of http://101.37.21.24:3385/datuodui_group/cwms into dev_pay_service
    # Conflicts:
    #	cwms-items/cwms-items-service/src/main/resources/application.properties
    #	mybatis-generator/src/main/resources/generator.properties
    #	mybatis-generator/src/main/resources/generatorConfig.xml

--- cwms-sso/clms-sso-service/src/main/java/com/lcdt/login/web/AuthController.java
+++ cwms-sso/clms-sso-service/src/main/java/com/lcdt/login/web/AuthController.java
@@ -13,11 +13,14 @@ import com.lcdt.userinfo.exception.DeptmentExistException;
 import com.lcdt.userinfo.exception.PassErrorException;
 import com.lcdt.userinfo.exception.UserNotExistException;
 import com.lcdt.userinfo.model.Company;
+import com.lcdt.userinfo.model.LoginLog;
 import com.lcdt.userinfo.model.User;
 import com.lcdt.userinfo.model.UserCompRel;
 import com.lcdt.userinfo.service.CompanyService;
 import com.lcdt.userinfo.service.CreateCompanyService;
+import com.lcdt.userinfo.service.LoginLogService;
 import com.lcdt.userinfo.service.UserService;
+import com.lcdt.util.HttpUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -243,6 +246,8 @@ public class AuthController {
 
 	}
 
+	@Reference(async = true)
+	LoginLogService loginLogService;
 
 	@RequestMapping("/logincompany")
 	@ExcludeIntercept(excludeIntercept = {CompanyInterceptorAbstract.class})
@@ -258,6 +263,14 @@ public class AuthController {
 		ticketService.generateTicketInResponse(request, response, userInfo.getUserId(), companyId);
 		LoginSessionReposity.setCompanyMemberInSession(request, companyMember);
 		strategy.hasAuthRedirect(request, response);
+
+		LoginLog loginLog = new LoginLog();
+		loginLog.setUserId(userInfo.getUserId());
+		loginLog.setLoginCompanyId(companyId);
+		loginLog.setLoginAgent("PC");
+		loginLog.setLoginIp(HttpUtils.getLocalIp(request));
+		loginLogService.saveLog(loginLog);
+
 		return null;
 	}
 
