becbaab0f684032a1ca721489a0d05e30368c2b0
liuhu <liuhu@lichendt.com>
2017-12-27 19:31:18 +0800

    定位功能完善

--- user-center/user-api/src/main/java/com/lcdt/userinfo/service/DriverService.java
+++ user-center/user-api/src/main/java/com/lcdt/userinfo/service/DriverService.java
@@ -13,4 +13,13 @@ public interface DriverService {
     int addDriver(Driver driver);
 
     List<Driver> getGpsInfo(List<String> driverPhoneList);
+
+    /**
+     * 修改定位状态（0 - 未开通  1 - 待激活 2 - 已激活）
+     * @param driver
+     * @return
+     */
+    int modGpsStatus(Driver driver);
+
+    int updateLocation(Driver driver);
 }
