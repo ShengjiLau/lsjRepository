20e2fdf2070387669557ee28b8a260813c6fbc54
mawei <mawei@lichendt.com>
2018-01-05 15:59:21 +0800

    loginlog 接口增加 用户名 手机号

--- clms-customer/clms-customer-service/src/test/java/com/lcdt/customer/service/impl/InviteCustomerServiceTest.java
+++ clms-customer/clms-customer-service/src/test/java/com/lcdt/customer/service/impl/InviteCustomerServiceTest.java
@@ -18,7 +18,7 @@ public class InviteCustomerServiceTest extends BaseSpringBootTest {
 	@Test
 	public void testSendEmail(){
 		SimpleMailMessage message = new SimpleMailMessage();
-		message.setFrom("737298251@qq.com");
+		message.setFrom("service@datuodui.com");
 		message.setTo("mawei@lichendt.com");
 		message.setSubject("这是一封测试邮件");
 		message.setText("这是一封测试邮件");
