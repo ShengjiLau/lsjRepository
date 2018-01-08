2600a0943a6a06a212943ef2da31df39996361a0
mawei <mawei@lichendt.com>
2018-01-08 10:55:06 +0800

    新增 公司服务剩余数量接口

--- nginx_conf/request_access.lua
+++ nginx_conf/request_access.lua
@@ -2,7 +2,7 @@
 if ngx.var.cookie_cwms_ticket == nil then
     --ngx.print(ngx.var.host .. ngx.var.port .. '/' .. ngx.var.request_uri)
     backurl = ngx.escape_uri("http://" .. ngx.req.get_headers()["Host"] .. '/' .. ngx.var.request_uri)     
-    ngx.redirect("http://39.107.12.215:52412/account/?authCallback=" .. backurl, 302)
+    ngx.redirect("http://39.107.12.215:52412/account/?auth_callback=" .. backurl, 302)
 end
 ngx.exit(ngx.OK)
 return      
