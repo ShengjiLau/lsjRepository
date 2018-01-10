60e66f32e3cb4394810b79c649c5a175333542f3
lyqishan <lyqishan@126.com>
2018-01-10 18:53:45 +0800

    修改

--- cwms-items/cwms-items-service/src/main/java/com/lcdt/items/web/controller/api/SubItemsInfoApi.java
+++ cwms-items/cwms-items-service/src/main/java/com/lcdt/items/web/controller/api/SubItemsInfoApi.java
@@ -9,6 +9,7 @@ import com.lcdt.items.model.SubItemsInfoDao;
 import com.lcdt.items.service.SubItemsInfoService;
 import com.lcdt.items.web.dto.GoodsListParamsDto;
 import com.lcdt.items.web.dto.PageBaseDto;
+import com.lcdt.util.ClmsBeanUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -59,9 +60,13 @@ public class SubItemsInfoApi {
 
     @ApiOperation("查询商品列表")
     @GetMapping("/goodslist")
-    public PageBaseDto<List<GoodsInfoDao>> queryGoodsList(GoodsListParamsDto params){
+    public PageBaseDto<List<GoodsInfoDao>> queryGoodsList(GoodsListParamsDto params,@ApiParam(value = "页码", required = true) @RequestParam Integer pageNo,
+                                                          @ApiParam(value = "每页显示条数", required = true) @RequestParam Integer pageSize){
         Long companyId=SecurityInfoGetter.getCompanyId();
-        Map map= new HashMap();
+        Map map= ClmsBeanUtil.beanToMap(params);
+        map.put("companyId",companyId);
+        map.put("pageNo",pageNo);
+        map.put("pageSize",pageSize);
         PageInfo<List<GoodsInfoDao>> listPageInfo=subItemsInfoService.queryByCondition(map);
         return new PageBaseDto(listPageInfo.getList(),listPageInfo.getTotal());
     }
