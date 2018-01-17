ad7b5ab61cff5352f9eb9aa1d3bfc380bf4af1a6
liuhu <liuhu@lichendt.com>
2018-01-17 17:06:58 +0800

    定位添加余额检查

---
+++ clms-traffic/clms-traffic-service/src/main/java/com/lcdt/traffic/util/BalanceCheckBo.java
@@ -0,0 +1,20 @@
+package com.lcdt.traffic.util;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.lcdt.pay.rpc.SmsCountService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @AUTHOR liuh
+ * @DATE 2018-01-16
+ */
+@Service
+public class BalanceCheckBo {
+
+    @Reference
+    public SmsCountService smsCountService;
+
+    public boolean check(Long companyId){
+        return smsCountService.checkProductCount(companyId,1,"gms_service");
+    }
+}
