2b7b6d9529d1500b20e1fdd78483fde58c86c762
mawei <mawei@lichendt.com>
2018-01-30 19:45:57 +0800

    fix bugs

--- clms-pay-service/pay-service/src/main/java/com/lcdt/pay/web/AlipayWebController.java
+++ clms-pay-service/pay-service/src/main/java/com/lcdt/pay/web/AlipayWebController.java
@@ -64,11 +64,12 @@ public class AlipayWebController {
 
     /**
      * 新建一个充值订单
+     *
      * @return
      */
-    @RequestMapping(value = "/topup",method = RequestMethod.POST)
+    @RequestMapping(value = "/topup", method = RequestMethod.POST)
     @ResponseBody
-    public PayOrder createTopupPayOrder(Integer amount){
+    public PayOrder createTopupPayOrder(Integer amount) {
         //这里的单位是元
         Long companyId = SecurityInfoGetter.getCompanyId();
         Long userId = SecurityInfoGetter.getUser().getUserId();
@@ -96,7 +97,7 @@ public class AlipayWebController {
 
         //获得初始化的AlipayClient
         AlipayClient alipayClient = new DefaultAlipayClient(AlipayContants.getApiGataway(), AlipayContants.getAppId()
-                , AlipayContants.getAppPrivateKey(), FORMAT, CHARSET, AlipayContants.getAlipayPublicKey(),"RSA2");
+                , AlipayContants.getAppPrivateKey(), FORMAT, CHARSET, AlipayContants.getAlipayPublicKey(), "RSA2");
 
         AlipayTradePagePayRequest alipayRequest = new AlipayTradePagePayRequest();//创建API对应的request
         AlipayTradeOrder alipayTradeOrder = new AlipayTradeOrder();
@@ -124,32 +125,17 @@ public class AlipayWebController {
     @ResponseBody
     public String alipayNotify(HttpServletRequest request) throws AlipayApiException, UnsupportedEncodingException {
 
-        HashMap<String, String> parameterMap = new HashMap<>();
-
-        Map<String,String[]> requestParams = request.getParameterMap();
-        for (Iterator<String> iter = requestParams.keySet().iterator(); iter.hasNext();) {
-            String name = (String) iter.next();
-            String[] values = (String[]) requestParams.get(name);
-            String valueStr = "";
-            for (int i = 0; i < values.length; i++) {
-                valueStr = (i == values.length - 1) ? valueStr + values[i]
-                        : valueStr + values[i] + ",";
-            }
-            //乱码解决，这段代码在出现乱码时使用
-            valueStr = new String(valueStr.getBytes("ISO-8859-1"), "utf-8");
-            parameterMap.put(name, valueStr);
-
-        }
-        logger.info("alipay 异步请求 {}",request.getRequestURI()+request.getQueryString());
+        Map<String, String> parameterMap = allRequestParamterMap(request);
+        boolean b = checkSignature(parameterMap);
 
         boolean signVerified = AlipaySignature.rsaCheckV1(parameterMap, AlipayContants.getAlipayPublicKey(), CHARSET, "RSA2");
 
         if (signVerified) {
-            String orderNo = new String(request.getParameter("out_trade_no").getBytes("ISO-8859-1"),"UTF-8");
+            String orderNo = new String(request.getParameter("out_trade_no").getBytes("ISO-8859-1"), "UTF-8");
             //支付宝交易号
-            String trade_orderNono = new String(request.getParameter("trade_no").getBytes("ISO-8859-1"),"UTF-8");
+            String trade_orderNono = new String(request.getParameter("trade_no").getBytes("ISO-8859-1"), "UTF-8");
             //交易状态
-            String tradeStatus = new String(request.getParameter("trade_status").getBytes("ISO-8859-1"),"UTF-8");
+            String tradeStatus = new String(request.getParameter("trade_status").getBytes("ISO-8859-1"), "UTF-8");
             logger.info("接受支付宝 支付推送 交易订单号：{}  支付状态 {} ", orderNo, tradeStatus);
 
             if (!"TRADE_SUCCESS".equals(tradeStatus)) {
@@ -164,7 +150,7 @@ public class AlipayWebController {
 
             orderService.changeToPayFinish(payOrder, OrderServiceImpl.PayType.ALIPAY);
             return "success";
-        }else{
+        } else {
             logger.info("异步回调延签失败");
         }
 
@@ -179,12 +165,36 @@ public class AlipayWebController {
      */
     @RequestMapping("/alipay/returnurl")
     @ResponseBody
-    public String alipayReturn(HttpServletRequest request) throws UnsupportedEncodingException {
+    public String alipayReturn(HttpServletRequest request) throws UnsupportedEncodingException, AlipayApiException {
+        Map<String, String> parameterMap = allRequestParamterMap(request);
+        boolean b = checkSignature(parameterMap);
+        String orderNo = parameterMap.get("out_trade_no");
+        String tradeStatus = parameterMap.get("trade_status");
+        if (b) {
+            PayOrder payOrder = orderService.selectByOrderNo(orderNo);
 
+            if (tradeStatus == null) {
+                return "success";
+            }
+
+            if (tradeStatus.equals("TRADE_SUCCESS")) {
+                if (payOrder == null) {
+                    logger.error("充值订单数据库没有记录 orderNo:  {} ", payOrder.getOrderNo());
+                    return "fail";
+                }
+                orderService.changeToPayFinish(payOrder, OrderServiceImpl.PayType.ALIPAY);
+            }
+            return "success";
+        } else {
+            return "fail";
+        }
+    }
+
+    public Map<String,String> allRequestParamterMap(HttpServletRequest request) throws UnsupportedEncodingException {
         HashMap<String, String> parameterMap = new HashMap<>();
 
-        Map<String,String[]> requestParams = request.getParameterMap();
-        for (Iterator<String> iter = requestParams.keySet().iterator(); iter.hasNext();) {
+        Map<String, String[]> requestParams = request.getParameterMap();
+        for (Iterator<String> iter = requestParams.keySet().iterator(); iter.hasNext(); ) {
             String name = (String) iter.next();
             String[] values = (String[]) requestParams.get(name);
             String valueStr = "";
@@ -195,48 +205,25 @@ public class AlipayWebController {
             //乱码解决，这段代码在出现乱码时使用
             valueStr = new String(valueStr.getBytes("ISO-8859-1"), "utf-8");
             parameterMap.put(name, valueStr);
-
+            logger.info("alipay 参数 {} {}", name, valueStr);
         }
-
-        String orderNo = parameterMap.get("out_trade_no");
-        String tradeStatus = parameterMap.get("trade_status");
-        try {
-            boolean b = AlipaySignature.rsaCheckV1(parameterMap, AlipayContants.getAlipayPublicKey(), CHARSET, "RSA2");
-            if (b) {
-                PayOrder payOrder = orderService.selectByOrderNo(orderNo);
-
-                if (tradeStatus == null) {
-                    return "success";
-                }
-
-                if (tradeStatus.equals("TRADE_SUCCESS")) {
-                    if (payOrder == null) {
-                        logger.error("充值订单数据库没有记录 orderNo:  {} ",payOrder.getOrderNo());
-                        return "fail";
-                    }
-                    orderService.changeToPayFinish(payOrder, OrderServiceImpl.PayType.ALIPAY);
-                }
-                return "success";
-            }else{
-                return "fail";
-            }
-        } catch (AlipayApiException e) {
-            e.printStackTrace();
-        }
-
-        return "fail";
+        return parameterMap;
     }
 
 
-    public void checkAlipayOrder(){
+    public boolean checkSignature(Map<String,String> parameterMap) throws UnsupportedEncodingException, AlipayApiException {
+       return AlipaySignature.rsaCheckV1(parameterMap, AlipayContants.getAlipayPublicKey(), CHARSET, "RSA2");
+    }
 
+
+    public void checkAlipayOrder() {
 
 
         return;
     }
 
     @RequestMapping("/wechatqrcode")
-    public void wechatQrcode(HttpServletResponse response,Long orderId,HttpServletRequest request){
+    public void wechatQrcode(HttpServletResponse response, Long orderId, HttpServletRequest request) {
         PayOrder serverOrder = orderService.selectByOrderId(orderId);
         Integer orderStatus = serverOrder.getOrderStatus();
         //判断是否是待支付状态
@@ -249,21 +236,21 @@ public class AlipayWebController {
         String qrcodeUrl = wechatPayApi.createWechatOrder(serverOrder.getOrderNo(), localIp, serverOrder.getOrderAmount());
 
 
-        if( qrcodeUrl ==null || "".equals(qrcodeUrl))
+        if (qrcodeUrl == null || "".equals(qrcodeUrl))
             return;
         MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
         Map hints = new HashMap();
         hints.put(EncodeHintType.CHARACTER_SET, "UTF-8"); //设置字符集编码类型
         BitMatrix bitMatrix = null;
         try {
-            bitMatrix = multiFormatWriter.encode(qrcodeUrl, BarcodeFormat.QR_CODE, 300, 300,hints);
+            bitMatrix = multiFormatWriter.encode(qrcodeUrl, BarcodeFormat.QR_CODE, 300, 300, hints);
             BufferedImage image = toBufferedImage(bitMatrix);
             //输出二维码图片流
             try {
 //                response.setHeader();
                 response.setContentType("image/png");
-                response.setHeader("Pragma","no-cache");
-                response.setHeader("Cache-Control","no-cache");
+                response.setHeader("Pragma", "no-cache");
+                response.setHeader("Cache-Control", "no-cache");
                 ImageIO.write(image, "png", response.getOutputStream());
             } catch (IOException e) {
                 e.printStackTrace();
@@ -273,15 +260,15 @@ public class AlipayWebController {
         }
     }
 
-    private static BufferedImage toBufferedImage(BitMatrix bitMatrix){
+    private static BufferedImage toBufferedImage(BitMatrix bitMatrix) {
         BufferedImage image = null;
         int width = bitMatrix.getWidth();
         int height = bitMatrix.getHeight();
-        image = new BufferedImage(width, height,BufferedImage.TYPE_INT_ARGB);
+        image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
                 image.setRGB(x, y, bitMatrix.get(x, y) == true ?
-                        Color.BLACK.getRGB():Color.WHITE.getRGB());
+                        Color.BLACK.getRGB() : Color.WHITE.getRGB());
             }
         }
 
