5e8d8adc91ad9392e8b1c2be429763163063710c
yangbinq <ybq_9861@163.com>
2018-02-06 08:06:35 +0800

    'ok'

--- clms-traffic/clms-traffic-service/src/main/java/com/lcdt/traffic/web/controller/api/SplitGoodsApi.java
+++ clms-traffic/clms-traffic-service/src/main/java/com/lcdt/traffic/web/controller/api/SplitGoodsApi.java
@@ -51,16 +51,6 @@ public class SplitGoodsApi {
 
 
 
-    @ApiOperation("派单-直派-信息拉取")
-    @RequestMapping(value = "/splitGoodsLoad4Direct",method = RequestMethod.POST)
-    @PreAuthorize("hasRole('ROLE_SYS_ADMIN') or hasAuthority('traffic_split_goods_load_4_direct')")
-    public String splitGoodsLoad4Direct(@ApiParam(value = "计划ID",required = true) @RequestParam Long waybillPlanId) {
-
-        return  null;
-    }
-
-
-
     @ApiOperation(value = "派单-直派")
     @RequestMapping(value = "/splitGoods4Direct",method = RequestMethod.POST)
     @PreAuthorize("hasRole('ROLE_SYS_ADMIN') or hasAuthority('traffic_split_goods_4_direct')")
