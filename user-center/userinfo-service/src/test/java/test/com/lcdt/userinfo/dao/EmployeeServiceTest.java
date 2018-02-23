1b256bfced20279127582782836eaf1ebd439dc6
mawei <mawei@lichendt.com>
2018-02-23 17:09:06 +0800

    fix bugs

---
+++ user-center/userinfo-service/src/test/java/test/com/lcdt/userinfo/dao/EmployeeServiceTest.java
@@ -0,0 +1,22 @@
+package test.com.lcdt.userinfo.dao;
+
+import com.lcdt.userinfo.service.impl.EmployeeServiceImpl;
+import com.lcdt.userinfo.web.dto.CreateEmployeeAccountDto;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+
+public class EmployeeServiceTest extends BaseIntegrationContext{
+
+    @Autowired
+    EmployeeServiceImpl employeeService;
+
+    @Test
+    @Rollback
+    public void testAddEmployee(){
+
+
+    }
+
+
+}
