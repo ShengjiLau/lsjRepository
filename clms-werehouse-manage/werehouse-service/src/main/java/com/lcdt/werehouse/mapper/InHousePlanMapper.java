668eb95a0194bf89ad5eb6ccdf82274d0c7ce030
mawei <mawei@lichendt.com>
2018-03-08 09:38:31 +0800

    init werehouse service

---
+++ clms-werehouse-manage/werehouse-service/src/main/java/com/lcdt/werehouse/mapper/InHousePlanMapper.java
@@ -0,0 +1,7 @@
+package com.lcdt.werehouse.mapper;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.lcdt.werehouse.entity.InHousePlan;
+
+public interface InHousePlanMapper extends BaseMapper<InHousePlan> {
+}
