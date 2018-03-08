1f9dbd1f39ced4fff83e46ffbe202efb5af1a6c9
liuhu <liuhu@lichendt.com>
2018-03-08 22:48:58 +0800

    分组及司机获取api

---
+++ clms-traffic/clms-traffic-service/src/main/java/com/lcdt/traffic/web/dto/OwnDriverDto2.java
@@ -0,0 +1,27 @@
+package com.lcdt.traffic.web.dto;
+
+/**
+ * @AUTHOR liuh
+ * @DATE 2018-03-08
+ */
+public class OwnDriverDto2 {
+
+    private Long ownDriverId;
+    private String driverName;
+
+    public Long getOwnDriverId() {
+        return ownDriverId;
+    }
+
+    public void setOwnDriverId(Long ownDriverId) {
+        this.ownDriverId = ownDriverId;
+    }
+
+    public String getDriverName() {
+        return driverName;
+    }
+
+    public void setDriverName(String driverName) {
+        this.driverName = driverName;
+    }
+}
