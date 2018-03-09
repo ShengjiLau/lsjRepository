ea6818c1b0089c73fce45a06ef57c726c37e9391
liuhu <liuhu@lichendt.com>
2018-03-09 16:55:55 +0800

    审批流程修改和删除功能

--- clms-contract/clms-contract-service/src/main/java/com/lcdt/contract/service/impl/ApprovalProcessServiceImpl.java
+++ clms-contract/clms-contract-service/src/main/java/com/lcdt/contract/service/impl/ApprovalProcessServiceImpl.java
@@ -61,11 +61,49 @@ public class ApprovalProcessServiceImpl implements ApprovalProcessService {
 
     @Override
     public int modApprovalProcess(ApprovalProcessDto approvalProcessDto) {
-        return 0;
+        /*分为三步部：1.先更新审批主表信息 2.删除之前保留的审批人信息 3.批量保存审批人和抄送人信息*/
+        ApprovalProcess approvalProcess = new ApprovalProcess();
+        BeanUtils.copyProperties(approvalProcessDto, approvalProcess);   //拷贝审批流程主表属性
+        int row = 0;
+        try {
+            row = approvalProcessMapper.updateByPrimaryKey(approvalProcess);
+            if (row > 0 && null != approvalProcess.getProcessId()) {    // 判断主表更新是否成功
+                if (null != approvalProcessDto.getProcessApproverList() && approvalProcessDto.getProcessApproverList().size() > 0) {
+                    //删除审批人
+                    processApproverMapper.delByProcessId(approvalProcess.getProcessId());
+                    for (ProcessApprover processApprover : approvalProcessDto.getProcessApproverList()) {
+                        processApprover.setProcessId(approvalProcess.getProcessId());   //循环设置关联的主表的主键processId
+                    }
+                    row += processApproverMapper.insertBatch(approvalProcessDto.getProcessApproverList());  //批量插入审批人信息
+                }
+            }else {
+                throw new RuntimeException("修改失败！");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("修改失败！");
+        }
+        return row;
     }
 
     @Override
-    public int delApprovalProcess(ApprovalProcessDto approvalProcessDto) {
-        return 0;
+    public int delApprovalProcess(Long processId,Long companyId) {
+        int row = 0;
+        try {
+            // 先删除审批人信息，在删除审批流程主表
+            row = processApproverMapper.delByProcessId(processId);
+            if(row > 0){
+                row += approvalProcessMapper.deleteByPrimaryKey(processId,companyId);
+            }else{
+                throw new RuntimeException("删除失败!");
+            }
+            if(row <= 0){
+                throw new RuntimeException("删除失败!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("删除失败!");
+        }
+        return row;
     }
 }
