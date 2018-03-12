d9640b58144639cfb50e2e0c56ad58bb5b2d54aa
mawei <mawei@lichendt.com>
2018-03-12 10:07:33 +0800

    bug fix

--- clms-pay-service/pay-service/src/main/java/com/lcdt/pay/utils/OrderNoGenerator.java
+++ clms-pay-service/pay-service/src/main/java/com/lcdt/pay/utils/OrderNoGenerator.java
@@ -1,20 +1,30 @@
 package com.lcdt.pay.utils;
 
+import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class OrderNoGenerator {
 
     public static String generatorOrderNo(){
-        String replace = UUID.randomUUID().toString().replace("-", "");
+        String replace = generateTimeNo();
         return replace;
     }
 
-    public static String generateTimeNo(Long Id){
-        LocalDate now = LocalDate.now();
-        String s = now.toString();
-        String replace = s.replace("-", "");
-        return replace+Id;
+    private static AtomicInteger atomicInteger = new AtomicInteger(0);
+
+    public static String generateTimeNo(){
+        int countNo = atomicInteger.addAndGet(1);
+
+        DecimalFormat df = new DecimalFormat("0000");
+        String str2 = df.format(countNo);
+
+        long timestamp = System.currentTimeMillis() / 1000;
+        return String.valueOf(timestamp) + str2;
     }
+
+
+
 
 }
