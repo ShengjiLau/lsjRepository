8d8a67e853f352bc85f72a102e41b18644e337b6
mawei <mawei@lichendt.com>
2018-03-16 13:57:03 +0800

    fix bug

--- nginx_conf/release/request_access_release.lua
+++ nginx_conf/release/request_access_release.lua
@@ -2,8 +2,8 @@
 if ngx.var.cookie_cwms_ticket == nil then
     --ngx.print(ngx.var.host .. ngx.var.port .. '/' .. ngx.var.request_uri)
     backurl = ngx.escape_uri("http://" .. ngx.req.get_headers()["Host"] .. '/' .. ngx.var.request_uri)     
-    ngx.redirect("http://39.107.12.215:52412/account/?auth_callback=" .. backurl, 302)
+    ngx.redirect("http://login.datuodui.com/account/?auth_callback=" .. backurl, 302)
 end
 ngx.exit(ngx.OK)
-return
+return      
 
