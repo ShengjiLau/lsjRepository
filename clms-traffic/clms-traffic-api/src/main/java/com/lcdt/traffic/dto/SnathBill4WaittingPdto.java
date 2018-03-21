5120e7720305c1a96ea9d11b0c51759b2f662abd
yangbinq <ybq_9861@163.com>
2018-03-21 11:15:51 +0800

    '抢单说明‘

--- clms-traffic/clms-traffic-api/src/main/java/com/lcdt/traffic/dto/SnathBill4WaittingPdto.java
+++ clms-traffic/clms-traffic-api/src/main/java/com/lcdt/traffic/dto/SnathBill4WaittingPdto.java
@@ -1,18 +1,29 @@
 package com.lcdt.traffic.dto;
 
+import io.swagger.annotations.ApiModelProperty;
+
 /**
  * Created by yangbinq on 2018/3/19.
  * Description: 待抢单条件
  */
 public class SnathBill4WaittingPdto implements java.io.Serializable {
 
+    @ApiModelProperty(value = "发布计划的企业ID集合，有多个id时用 , 隔开")
     private String compyIds; //发布计划的企业ID集合
+
+    @ApiModelProperty(value = "排序字段， start_date--起运时间；arrive_date--显示到达时间")
     private String orderFiled; //排序字段
+
+    @ApiModelProperty(value = "排序方式desc/asc")
     private String orderDesc;  //排序方式
+
+    @ApiModelProperty(value = "登录司机ID")
     private Long driverId; //登录司机ID
 
-
+    @ApiModelProperty(value = "页码")
     private int pageNo; //分页（第几页）
+
+    @ApiModelProperty(value = "每页显示条数")
     private int pageSize;//每页多少
 
 
