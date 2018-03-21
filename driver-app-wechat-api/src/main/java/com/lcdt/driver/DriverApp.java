a706f104af68388d215dc0dddbfcf652bc19a5e1
lyqishan <lyqishan@126.com>
2018-03-21 19:00:21 +0800

    司机运单接口修改

--- driver-app-wechat-api/src/main/java/com/lcdt/driver/DriverApp.java
+++ driver-app-wechat-api/src/main/java/com/lcdt/driver/DriverApp.java
@@ -1,13 +1,16 @@
 package com.lcdt.driver;
 
 import com.lcdt.clms.security.annontion.EnableTokenBaseSecurity;
+import com.lcdt.converter.ClmsResponseConvertConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableTokenBaseSecurity
 @EnableSwagger2
+@Import({ClmsResponseConvertConfig.class})
 public class DriverApp {
 
     public static void main(String[] args) {
