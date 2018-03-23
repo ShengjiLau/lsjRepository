4a31083f451464fe1c8aad4ffa06e83a1f44b3a7
lyqishan <lyqishan@126.com>
2018-03-23 19:43:00 +0800

    运单统计

--- clms-traffic/clms-traffic-service/src/main/java/com/lcdt/traffic/dao/IndexOverviewMapper.java
+++ clms-traffic/clms-traffic-service/src/main/java/com/lcdt/traffic/dao/IndexOverviewMapper.java
@@ -36,6 +36,20 @@ public interface IndexOverviewMapper {
     List<Map<String,Object>> selectOwnWaybillData(Map map);
 
     /**
+     * 运单统计
+     * @param map
+     * @return
+     */
+    Map<String,Object> selectCustomerWaybill(Map map);
+
+    /**
+     * 运单统计详情数据
+     * @param map
+     * @return
+     */
+    List<Map<String,Object>> selectCustomerWaybillData(Map map);
+
+    /**
      * 执行中的计划和在途运单统计
      * @param companyId
      * @return
