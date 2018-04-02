d8f3b65ca937b15f86d219e58ebe65d2600b2088
liuhu <liuhu@lichendt.com>
2018-04-02 14:16:49 +0800

    审批列表增加产品信息列表返回

--- clms-contract/clms-contract-service/src/main/java/com/lcdt/contract/web/dto/OrderApprovalDto.java
+++ clms-contract/clms-contract-service/src/main/java/com/lcdt/contract/web/dto/OrderApprovalDto.java
@@ -2,12 +2,22 @@ package com.lcdt.contract.web.dto;
 
 import com.lcdt.contract.model.Order;
 import com.lcdt.contract.model.OrderApproval;
+import com.lcdt.contract.model.OrderProduct;
 
 import java.util.List;
 
 public class OrderApprovalDto extends Order{
 
+    private List<OrderProduct> orderProductList;
     private List<OrderApproval> orderApprovalList;
+
+    public List<OrderProduct> getOrderProductList() {
+        return orderProductList;
+    }
+
+    public void setOrderProductList(List<OrderProduct> orderProductList) {
+        this.orderProductList = orderProductList;
+    }
 
     public List<OrderApproval> getOrderApprovalList() {
         return orderApprovalList;
