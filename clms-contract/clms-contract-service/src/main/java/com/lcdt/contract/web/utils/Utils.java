f5a569caccb0393ed539eb4642fe31a8c459920c
lizhou <lizhou@lichendt.com>
2018-04-08 17:33:02 +0800

    合同状态判断

--- clms-contract/clms-contract-service/src/main/java/com/lcdt/contract/web/utils/Utils.java
+++ clms-contract/clms-contract-service/src/main/java/com/lcdt/contract/web/utils/Utils.java
@@ -11,6 +11,10 @@ import java.util.Date;
 public class Utils {
     //根据合同生效日期判断合同状态
     public static ContractDto getContractStatus(ContractDto dto){
+        int isApproval = 0;//不需要审批
+        if(dto.getContractApprovalList() != null && dto.getContractApprovalList().size() > 0){
+            isApproval = 1;//需要审批
+        }
         try{
             SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
             Date nowDate = sdf.parse(sdf.format(new Date()));//当前日期
@@ -18,26 +22,31 @@ public class Utils {
                 Date startDate = sdf.parse(sdf.format(dto.getStartDate()));
                 Date endDate = sdf.parse(sdf.format(dto.getEndDate()));
                 if(startDate.getTime() <= nowDate.getTime() && nowDate.getTime() <= endDate.getTime()){
-                    dto.setContractStatus((short)0);
+                    dto.setContractStatus(isApproval==0?(short)0:(short)2);
                 }else{
-                    dto.setContractStatus((short)2);
+                    if(startDate.getTime() > nowDate.getTime()) {
+                        dto.setContractStatus(isApproval == 0 ? (short) 1 : (short) 2);
+                    }
+                    if(nowDate.getTime() > endDate.getTime()) {
+                        dto.setContractStatus(isApproval==0?(short)3:(short)2);
+                    }
                 }
             }else if(dto.getStartDate() != null && dto.getEndDate() == null){
                 Date startDate = sdf.parse(sdf.format(dto.getStartDate()));
                 if(startDate.getTime() > nowDate.getTime()){
-                    dto.setContractStatus((short)2);
+                    dto.setContractStatus(isApproval==0?(short)1:(short)2);
                 }else{
-                    dto.setContractStatus((short)0);
+                    dto.setContractStatus(isApproval==0?(short)0:(short)2);
                 }
             }else if(dto.getStartDate() == null && dto.getEndDate() != null){
                 Date endDate = sdf.parse(sdf.format(dto.getEndDate()));
                 if(nowDate.getTime() > endDate.getTime()){
-                    dto.setContractStatus((short)3);
+                    dto.setContractStatus(isApproval==0?(short)3:(short)2);
                 }else{
-                    dto.setContractStatus((short)2);
+                    dto.setContractStatus(isApproval==0?(short)0:(short)2);
                 }
             }else{
-                dto.setContractStatus((short)2);
+                dto.setContractStatus(isApproval==0?(short)1:(short)2);//没有生效时间，不需要审批为待生效
             }
         }catch (Exception e){
             e.printStackTrace();
