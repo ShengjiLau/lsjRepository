4ece5b7cee67ece50b7addbae435be58c1c8b864
mawei <mawei@lichendt.com>
2018-04-08 16:26:47 +0800

    设置dubbo 调用超时时间

--- driver-app-wechat-api/src/main/java/com/lcdt/driver/dto/ForgetPwdDto.java
+++ driver-app-wechat-api/src/main/java/com/lcdt/driver/dto/ForgetPwdDto.java
@@ -1,4 +1,34 @@
 package com.lcdt.driver.dto;
 
 public class ForgetPwdDto {
+
+    String validCode;
+
+    String userName;
+
+    String pwd;
+
+    public String getValidCode() {
+        return validCode;
+    }
+
+    public void setValidCode(String validCode) {
+        this.validCode = validCode;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
 }
