a77ba321a3ea3e16c3045b1cbc59395f8d46dc7b
xuerongrong <xuerongrong@lichendt.com>
2018-04-09 13:46:42 +0800

    管车宝-企业客户-增加项目组逻辑

--- driver-app-wechat-api/src/main/java/com/lcdt/driver/wechat/api/DriverManageApi/CustomerApi.java
+++ driver-app-wechat-api/src/main/java/com/lcdt/driver/wechat/api/DriverManageApi/CustomerApi.java
@@ -5,6 +5,8 @@ import com.lcdt.clms.security.helper.TokenSecurityInfoGetter;
 import com.lcdt.customer.model.Customer;
 import com.lcdt.customer.rpcservice.CustomerRpcService;
 import com.lcdt.driver.dto.PageBaseDto;
+import com.lcdt.userinfo.model.Group;
+import com.lcdt.userinfo.service.UserGroupService;
 import io.swagger.annotations.Api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,13 +29,33 @@ import java.util.Map;
 public class CustomerApi {
     @Reference
     private CustomerRpcService customerService;
+    @Reference
+    private UserGroupService userGroupService;
 
     Logger logger = LoggerFactory.getLogger(CustomerApi.class);
     @GetMapping(value = "/list")
     private PageBaseDto<Customer> carrierCustomer(){
         Long companyId = TokenSecurityInfoGetter.getUserCompRel().getCompany().getCompId();//  获取companyId
+        Long userId = TokenSecurityInfoGetter.getUser().getUserId();//  获取companyId
         Map map = new HashMap();
         map.put("companyId", companyId);
+
+        StringBuffer sb = new StringBuffer();
+        List<Group> groupList =  userGroupService.userGroups(userId, companyId);
+        if(groupList!=null && groupList.size()>0) {
+            sb.append("(");
+            for(int i=0;i<groupList.size();i++) {
+                Group group = groupList.get(i);
+                sb.append(" find_in_set('"+group.getGroupId()+"',group_ids)");
+                if(i!=groupList.size()-1){
+                    sb.append(" or ");
+                }
+            }
+            sb.append(")");
+        }
+        map.put("groupIds", sb.toString());
+
+
         List<Customer> customerList = customerService.findBindCompanyIds(map);
         return new PageBaseDto<Customer>(customerList);
     }
