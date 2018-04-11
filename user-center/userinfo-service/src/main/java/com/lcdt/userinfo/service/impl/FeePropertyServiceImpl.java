a15584d8f2d3f19bb34c484017cb174c01e878c7
lizhou <lizhou@lichendt.com>
2018-04-11 09:48:52 +0800

    移动费用类型到用户服务

---
+++ user-center/userinfo-service/src/main/java/com/lcdt/userinfo/service/impl/FeePropertyServiceImpl.java
@@ -0,0 +1,51 @@
+package com.lcdt.userinfo.service.impl;
+
+import com.github.pagehelper.PageInfo;
+import com.lcdt.userinfo.dao.FeePropertyMapper;
+import com.lcdt.userinfo.model.FeeProperty;
+import com.lcdt.userinfo.service.FeePropertyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by liz on 2018/3/29.
+ */
+@Service
+public class FeePropertyServiceImpl implements FeePropertyService {
+
+    @Autowired
+    private FeePropertyMapper feePropertyMapper;
+
+    @Override
+    public PageInfo feePropertyList(Map m) {
+        List<FeeProperty> list = feePropertyMapper.selectByCondition(m);
+        PageInfo pageInfo = new PageInfo(list);
+        return pageInfo;
+    }
+
+    @Override
+    public int addFeeProperty(FeeProperty feeProperty) {
+        int result = feePropertyMapper.insert(feeProperty);
+        return result;
+    }
+
+    @Override
+    public int modifyFeeProperty(FeeProperty feeProperty) {
+        int result = feePropertyMapper.updateByPrimaryKey(feeProperty);
+        return result;
+    }
+
+    @Override
+    public int modifyFeePropertyIsDelete(Long proId) {
+        int result = feePropertyMapper.updateIsDeleteByPrimaryKey(proId);
+        return result;
+    }
+    @Override
+    public List<FeeProperty> getFeePropertyList(Map m){
+        List<FeeProperty> list = feePropertyMapper.selectByCondition(m);
+        return list;
+    }
+}
