ae521d6e17d51f96bffc492bbe917cd2e49b6d8e
yangbinq <ybq_9861@163.com>
2018-04-12 11:37:33 +0800

    'caiwu'

--- clms-traffic/clms-traffic-api/src/main/java/com/lcdt/traffic/dto/FeeFlow4SearchResultDto.java
+++ clms-traffic/clms-traffic-api/src/main/java/com/lcdt/traffic/dto/FeeFlow4SearchResultDto.java
@@ -6,16 +6,110 @@ package com.lcdt.traffic.dto;
 
 import io.swagger.annotations.ApiModelProperty;
 
+import java.util.Date;
+
 /**
  * Created by yangbinq on 2018/4/12.
  * Description:应收、应付列表查返回结果集合
  */
 public class FeeFlow4SearchResultDto implements java.io.Serializable {
 
+    @ApiModelProperty("业务流水号")
+    private String flowCode;
 
+    @ApiModelProperty("记录时间")
+    private Date createDate;
 
+    @ApiModelProperty("业务组")
+    private String groupName;
 
+    @ApiModelProperty("运单号")
+    private String waybillCode;
 
+    @ApiModelProperty("对帐单号")
+    private String reconcileCode;
 
+    @ApiModelProperty("收付款方")
+    private String name;
 
+    @ApiModelProperty("费用类型")
+    private String feeProperty;
+
+    @ApiModelProperty("最新费用")
+    private Float money;
+
+    @ApiModelProperty("原始费用")
+    private Float originalMoney;
+
+    public String getFlowCode() {
+        return flowCode;
+    }
+
+    public void setFlowCode(String flowCode) {
+        this.flowCode = flowCode;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public String getWaybillCode() {
+        return waybillCode;
+    }
+
+    public void setWaybillCode(String waybillCode) {
+        this.waybillCode = waybillCode;
+    }
+
+    public String getReconcileCode() {
+        return reconcileCode;
+    }
+
+    public void setReconcileCode(String reconcileCode) {
+        this.reconcileCode = reconcileCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFeeProperty() {
+        return feeProperty;
+    }
+
+    public void setFeeProperty(String feeProperty) {
+        this.feeProperty = feeProperty;
+    }
+
+    public Float getMoney() {
+        return money;
+    }
+
+    public void setMoney(Float money) {
+        this.money = money;
+    }
+
+    public Float getOriginalMoney() {
+        return originalMoney;
+    }
+
+    public void setOriginalMoney(Float originalMoney) {
+        this.originalMoney = originalMoney;
+    }
 }
--- clms-traffic/clms-traffic-service/src/main/java/com/lcdt/traffic/web/controller/api/FeeFlowApi.java
+++ clms-traffic/clms-traffic-service/src/main/java/com/lcdt/traffic/web/controller/api/FeeFlowApi.java
@@ -1,15 +1,21 @@
 package com.lcdt.traffic.web.controller.api;
 
+import com.github.pagehelper.PageInfo;
+import com.github.pagehelper.util.StringUtil;
+import com.lcdt.clms.security.helper.SecurityInfoGetter;
 import com.lcdt.traffic.dto.FeeFlow4SearchParamsDto;
+import com.lcdt.traffic.service.IFeeFlowService;
 import com.lcdt.traffic.web.dto.PageBaseDto;
+import com.lcdt.userinfo.model.Group;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created by yangbinq on 2018/4/12.
@@ -19,18 +25,37 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(value = "记账流水",description = "记账流水")
 public class FeeFlowApi {
 
-
-
+    @Autowired
+    private IFeeFlowService iFeeFlowService;
 
     @ApiOperation("应收/应付列表")
     @RequestMapping(value = "/search/list",method = RequestMethod.GET)
     @PreAuthorize("hasRole('ROLE_SYS_ADMIN')")
-    public PageBaseDto searchList(FeeFlow4SearchParamsDto dto,
-                                                @ApiParam(value = "页码",required = true,defaultValue = "1") @RequestParam Integer pageNo,
-                                                @ApiParam(value = "每页显示条数",required = true,defaultValue = "10") @RequestParam Integer pageSize) {
-
-
-        PageBaseDto pg_result = new PageBaseDto(null, 1);
+    public PageBaseDto searchList(FeeFlow4SearchParamsDto dto) {
+         StringBuffer sb = new StringBuffer();
+         if (dto.getGroupId()>0) {//业务组
+            dto.setGroupIds(dto.getGroupIds());
+        } else {
+            List<Group> groupList = SecurityInfoGetter.groups();
+             for(int i=0;i<groupList.size();i++) {
+                 Group group = groupList.get(i);
+                 sb.append(group.getGroupId());
+                 if(i!=groupList.size()-1){
+                    sb.append(",");
+                 }
+            }
+           dto.setGroupIds(sb.toString());
+        }
+        if (!StringUtils.isEmpty(dto.getCreateBegin())) {
+             dto.setCreateBegin(dto.getCreateBegin()+" 00:00:00");
+        }
+        if (!StringUtils.isEmpty(dto.getCreateEnd())) {
+            dto.setCreateEnd(dto.getCreateEnd()+" 23:59:59");
+        }
+        dto.setCompanyId(SecurityInfoGetter.getCompanyId());
+        dto.setIsDeleted((short)0);
+        PageInfo pg = iFeeFlowService.feeFlowList(dto);
+        PageBaseDto pg_result = new PageBaseDto(pg.getList(), pg.getTotal());
         return pg_result;
     }
 
