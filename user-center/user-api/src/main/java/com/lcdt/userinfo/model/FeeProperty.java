fbc415dff3cf3af74de4fe094d6172f7b9f0ed97
lizhou <lizhou@lichendt.com>
2018-04-24 14:20:47 +0800

    财务

--- user-center/user-api/src/main/java/com/lcdt/userinfo/model/FeeProperty.java
+++ user-center/user-api/src/main/java/com/lcdt/userinfo/model/FeeProperty.java
@@ -1,5 +1,7 @@
 package com.lcdt.userinfo.model;
 
+import io.swagger.annotations.ApiModelProperty;
+
 import java.io.Serializable;
 import java.util.Date;
 
@@ -7,24 +9,34 @@ import java.util.Date;
  * @author 
  */
 public class FeeProperty implements Serializable {
+    @ApiModelProperty(value="主键id")
     private Long proId;
 
+    @ApiModelProperty(value="0-运输，1-仓储")
     private Short type;
 
+    @ApiModelProperty(value="费用类型名称")
     private String name;
 
+    @ApiModelProperty(value="0-应收，1-应付")
     private Short isReceivable;
 
+    @ApiModelProperty(value="0-默认显示，1-默认隐藏")
     private Short isShow;
 
+    @ApiModelProperty(value="企业id")
     private Long companyId;
 
+    @ApiModelProperty(value="操作人id")
     private Long operatorId;
 
+    @ApiModelProperty(value="操作人姓名")
     private String operatorName;
 
+    @ApiModelProperty(value="创建时间")
     private Date createDate;
 
+    @ApiModelProperty(value="0-未删除，1-已删除")
     private Short isDeleted;
 
     private static final long serialVersionUID = 1L;
