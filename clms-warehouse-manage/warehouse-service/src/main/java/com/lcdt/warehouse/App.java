f7b2a72bebd0b7f64bbefb0b00ac354456e31d29
yangbinq <ybq_9861@163.com>
2018-05-08 10:51:41 +0800

    '仓储引入dubbo'

--- clms-warehouse-manage/warehouse-service/src/main/java/com/lcdt/warehouse/App.java
+++ clms-warehouse-manage/warehouse-service/src/main/java/com/lcdt/warehouse/App.java
@@ -1,11 +1,17 @@
 package com.lcdt.warehouse;
 
+import com.lcdt.clms.security.annontion.EnableClmsSecurity;
+import com.lcdt.converter.ClmsResponseConvertConfig;
+import com.lcdt.wms.config.DubboConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@Import({com.lcdt.swagger.SwaggerConfig.class, DubboConfig.class, ClmsResponseConvertConfig.class})
+@EnableClmsSecurity
 public class App {
 
     public static void main(String[] args) {
