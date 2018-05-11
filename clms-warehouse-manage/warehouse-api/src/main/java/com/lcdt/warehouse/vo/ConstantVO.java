2a85d29c2fc4ecb23905c57e921181c1edbc301d
lyqishan <lyqishan@126.com>
2018-05-11 10:19:29 +0800

    入库单状态

--- clms-warehouse-manage/warehouse-api/src/main/java/com/lcdt/warehouse/vo/ConstantVO.java
+++ clms-warehouse-manage/warehouse-api/src/main/java/com/lcdt/warehouse/vo/ConstantVO.java
@@ -8,6 +8,6 @@ public class ConstantVO {
 
     //入库单状态
     public static int IN_ORDER_STATUS_WATIE_STORAGE=1; //待入库
-    public static int IN_ORDER_STATUS_HAVE_STORAGE=2;//已入厂
+    public static int IN_ORDER_STATUS_HAVE_STORAGE=2;//已入库
     public static int IN_ORDER_STATUS_HAVE_CANCEL=3;//已取消
 }
