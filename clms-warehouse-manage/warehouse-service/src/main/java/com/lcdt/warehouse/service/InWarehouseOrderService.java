52f716429b833d50b5b60a53ffbdd49b269c46ab
lyqishan <lyqishan@126.com>
2018-05-11 15:15:35 +0800

    入库单

--- clms-warehouse-manage/warehouse-service/src/main/java/com/lcdt/warehouse/service/InWarehouseOrderService.java
+++ clms-warehouse-manage/warehouse-service/src/main/java/com/lcdt/warehouse/service/InWarehouseOrderService.java
@@ -4,8 +4,12 @@ import com.baomidou.mybatisplus.plugins.Page;
 import com.baomidou.mybatisplus.service.IService;
 import com.lcdt.warehouse.dto.InWarehouseOrderDto;
 import com.lcdt.warehouse.dto.InWarehouseOrderSearchParamsDto;
+import com.lcdt.warehouse.dto.InorderGoodsInfoDto;
 import com.lcdt.warehouse.dto.ModifyInOrderStatusParamsDto;
 import com.lcdt.warehouse.entity.InWarehouseOrder;
+import com.lcdt.warehouse.entity.InorderGoodsInfo;
+
+import java.util.List;
 
 /**
  * <p>
@@ -32,5 +36,7 @@ public interface InWarehouseOrderService extends IService<InWarehouseOrder> {
 
 
     boolean modifyInOrderStatus(ModifyInOrderStatusParamsDto params);
+
+    boolean storage(ModifyInOrderStatusParamsDto modifyParams, List<InorderGoodsInfoDto> listParams);
 
 }
