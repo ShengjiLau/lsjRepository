2fe3239771d30821a147a5d4e7c4193c585f1450
mawei <mawei@lichendt.com>
2018-05-18 16:54:46 +0800

    api json 返回格式修改

--- clms-warehouse-manage/warehouse-service/src/main/java/com/lcdt/warehouse/dto/InventoryQueryDto.java
+++ clms-warehouse-manage/warehouse-service/src/main/java/com/lcdt/warehouse/dto/InventoryQueryDto.java
@@ -19,6 +19,39 @@ public class InventoryQueryDto extends PageQueryDto {
     @ApiModelProperty(value = "客户id")
     private Long customerId;
 
+    @ApiModelProperty(value = "商品名称")
+    private String goodsName;
+
+    @ApiModelProperty(value = "商品条码")
+    private String goodsBarCode;
+
+    @ApiModelProperty(value = "商品编码")
+    private String goodsCode;
+
+    public String getGoodsName() {
+        return goodsName;
+    }
+
+    public void setGoodsName(String goodsName) {
+        this.goodsName = goodsName;
+    }
+
+    public String getGoodsBarCode() {
+        return goodsBarCode;
+    }
+
+    public void setGoodsBarCode(String goodsBarCode) {
+        this.goodsBarCode = goodsBarCode;
+    }
+
+    public String getGoodsCode() {
+        return goodsCode;
+    }
+
+    public void setGoodsCode(String goodsCode) {
+        this.goodsCode = goodsCode;
+    }
+
     public Long getWareHouseId() {
         return wareHouseId;
     }
