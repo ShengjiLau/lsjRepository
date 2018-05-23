20557de7b0d3a741e51cbfacda8a26d5e17ba3a7
lizhou <lizhou@lichendt.com>
2018-05-23 12:00:31 +0800

    rpc新增仓库列表

--- clms-warehouse-manage/warehouse-service/src/main/java/com/lcdt/warehouse/rpc/impl/WarehouseRpcServiceImpl.java
+++ clms-warehouse-manage/warehouse-service/src/main/java/com/lcdt/warehouse/rpc/impl/WarehouseRpcServiceImpl.java
@@ -1,6 +1,11 @@
 package com.lcdt.warehouse.rpc.impl;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.alibaba.dubbo.config.annotation.Service;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.lcdt.userinfo.rpc.GroupWareHouseRpcService;
+import com.lcdt.warehouse.dto.WarehouseDto;
 import com.lcdt.warehouse.entity.Warehouse;
 import com.lcdt.warehouse.entity.WarehouseLinkman;
 import com.lcdt.warehouse.entity.WarehouseLoc;
@@ -9,7 +14,9 @@ import com.lcdt.warehouse.mapper.WarehousseLocMapper;
 import com.lcdt.warehouse.mapper.WarehousseMapper;
 import com.lcdt.warehouse.rpc.WarehouseRpcService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.tl.commons.util.StringUtility;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -24,6 +31,8 @@ public class WarehouseRpcServiceImpl implements WarehouseRpcService{
     WarehousseLinkmanMapper warehousseLinkmanMapper;
     @Autowired
     WarehousseLocMapper warehousseLocMapper;
+    @Reference
+    GroupWareHouseRpcService groupWareHouseRpcService;
 
     @Override
     public Warehouse selectByPrimaryKey(Long whId) {
@@ -58,5 +67,37 @@ public class WarehouseRpcServiceImpl implements WarehouseRpcService{
     @Override
     public List<Warehouse> selectNotInWhIds(Map map){
         return warehousseMapper.selectNotInWhIds(map);
+    }
+
+    @Override
+    public PageInfo warehouseList(Map m){
+        int pageNo = 1;
+        int pageSize = 0; //0表示所有
+
+        if (m.containsKey("page_no")) {
+            if (m.get("page_no") != null) {
+                pageNo = (Integer) m.get("page_no");
+            }
+        }
+        if (m.containsKey("page_size")) {
+            if (m.get("page_size") != null) {
+                pageSize = (Integer) m.get("page_size");
+            }
+        }
+        PageHelper.startPage(pageNo, pageSize);
+        List<WarehouseDto> list = warehousseMapper.selectByCondition(m);
+        if(list != null && list.size() > 0){
+            Map map = new HashMap();
+            for(WarehouseDto dto : list){
+                if(StringUtility.isNotEmpty(dto.getGroupIds())){
+                    map.put("groupIds", dto.getGroupIds());
+                    map.put("wareHouseId", dto.getWhId());
+                    String groupNames = groupWareHouseRpcService.selectGroupNamesByGroupIds(map);
+                    dto.setGroupNames(groupNames);
+                }
+            }
+        }
+        PageInfo pageInfo = new PageInfo(list);
+        return pageInfo;
     }
 }
