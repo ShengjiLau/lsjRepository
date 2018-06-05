c261219435a4d3766ad88497edae5918a40e9878
yangbinq <ybq_9861@163.com>
2018-06-05 14:37:58 +0800

    '计划创建路由‘

--- clms-traffic/clms-traffic-service/src/main/java/com/lcdt/traffic/service/impl/Plan4CreateServiceImpl.java
+++ clms-traffic/clms-traffic-service/src/main/java/com/lcdt/traffic/service/impl/Plan4CreateServiceImpl.java
@@ -276,16 +276,18 @@ public class Plan4CreateServiceImpl implements Plan4CreateService {
                         }
                 }
 
-                //router:发布
-                Timeline event = new Timeline();
-                event.setActionTitle("【计划发布】（操作人："+dto.getCompanyName()+" "+vo.getCreateName()+"）");
-                event.setActionTime(new Date());
-                event.setCompanyId(vo.getCompanyId());
-                event.setSearchkey("R_PLAN");
-                event.setDataid(vo.getWaybillPlanId());
-                producer.noteRouter(event);
+
             }
         }
+
+        //router:发布
+        Timeline event = new Timeline();
+        event.setActionTitle("【计划发布】（操作人："+dto.getCompanyName()+" "+vo.getCreateName()+"）");
+        event.setActionTime(new Date());
+        event.setCompanyId(vo.getCompanyId());
+        event.setSearchkey("R_PLAN");
+        event.setDataid(vo.getWaybillPlanId());
+        producer.noteRouter(event);
         return vo;
     }
 
