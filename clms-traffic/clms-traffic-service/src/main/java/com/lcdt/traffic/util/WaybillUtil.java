63ecb6c6332e80f1d2712968626a1455bc077267
xuerongrong <xuerongrong@lichendt.com>
2018-06-07 16:02:17 +0800

    还原运单路由丢失的代码

--- clms-traffic/clms-traffic-service/src/main/java/com/lcdt/traffic/util/WaybillUtil.java
+++ clms-traffic/clms-traffic-service/src/main/java/com/lcdt/traffic/util/WaybillUtil.java
@@ -7,16 +7,16 @@ import java.util.Map;
  * Created by xrr on 2018/5/31.
  */
 public class WaybillUtil {
-    public final static Map<String,String> map_waybill_status = new HashMap<String,String>() {
+    public final static Map<Short,String> map_waybill_status = new HashMap<Short,String>() {
         {
-            put("1","运单生成"); //
-            put("2","入厂");//
-            put("3","出库");//
-            put("4","发货"); //
-            put("5","卸货"); //
-            put("6","签收确认");//
-            put("7","运单完成"); //
-            put("8","运单取消"); //
+            put((short)1,"运单生成"); //
+            put((short)2,"入厂");//
+            put((short)3,"出库");//
+            put((short)4,"发货"); //
+            put((short)5,"卸货"); //
+            put((short)6,"签收确认");//
+            put((short)7,"运单完成"); //
+            put((short)8,"运单取消"); //
         }
     };
 }
