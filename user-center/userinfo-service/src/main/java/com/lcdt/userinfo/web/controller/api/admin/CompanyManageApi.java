ee5f18234d4ae4d8f26d97cbb736bf704f8242b9
mawei <mawei@lichendt.com>
2018-06-11 16:49:40 +0800

    后台管理 用户接口

--- user-center/userinfo-service/src/main/java/com/lcdt/userinfo/web/controller/api/admin/CompanyManageApi.java
+++ user-center/userinfo-service/src/main/java/com/lcdt/userinfo/web/controller/api/admin/CompanyManageApi.java
@@ -3,5 +3,5 @@ package com.lcdt.userinfo.web.controller.api.admin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class CompanyApi {
+public class CompanyManageApi {
 }
