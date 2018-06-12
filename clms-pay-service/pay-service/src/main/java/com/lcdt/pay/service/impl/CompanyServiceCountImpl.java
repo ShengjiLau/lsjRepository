ec1e75767c4dc78d52884f984613a961c8662ff7
mawei <mawei@lichendt.com>
2018-06-12 18:04:47 +0800

    bug fix

--- clms-pay-service/pay-service/src/main/java/com/lcdt/pay/service/impl/CompanyServiceCountImpl.java
+++ clms-pay-service/pay-service/src/main/java/com/lcdt/pay/service/impl/CompanyServiceCountImpl.java
@@ -6,7 +6,6 @@ import com.lcdt.pay.rpc.CompanyServiceCountService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
