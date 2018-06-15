00a43cf3393b18f36bc1d459d102c049a0dc2d56
mawei <mawei@lichendt.com>
2018-06-15 18:18:17 +0800

    增加 业务描述

--- clms-warehouse-manage/warehouse-service/src/main/java/com/lcdt/warehouse/utils/CommonUtils.java
+++ clms-warehouse-manage/warehouse-service/src/main/java/com/lcdt/warehouse/utils/CommonUtils.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 public class CommonUtils {
 
+    //两浮点数相减 直接减会丢失精度
     public static Float subtractFloat(Float a, Float b) {
         BigDecimal b1 = new BigDecimal(Float.toString(a));
         BigDecimal b2 = new BigDecimal(Float.toString(b));
