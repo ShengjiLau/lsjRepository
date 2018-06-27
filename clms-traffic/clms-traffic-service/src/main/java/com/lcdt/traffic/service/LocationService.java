859c57dfc444dabbb7881bc2af9ebf8e436a8678
liuhu <liuhu@lichendt.com>
2018-06-27 09:19:53 +0800

    基站定位抽取service方法

---
+++ clms-traffic/clms-traffic-service/src/main/java/com/lcdt/traffic/service/LocationService.java
@@ -0,0 +1,17 @@
+package com.lcdt.traffic.service;
+
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * @AUTHOR liuh
+ * @DATE 2018-06-27
+ */
+public interface LocationService {
+
+    /**
+     * 基站定位信息查询，并返回相关信息
+     * @param mobile
+     * @return
+     */
+    JSONObject queryLocation(String mobile);
+}
--- clms-traffic/clms-traffic-service/src/main/java/com/lcdt/traffic/web/controller/api/LocationServiceApi.java
+++ clms-traffic/clms-traffic-service/src/main/java/com/lcdt/traffic/web/controller/api/LocationServiceApi.java
@@ -8,6 +8,7 @@ import com.lcdt.clms.security.helper.SecurityInfoGetter;
 import com.lcdt.traffic.config.BaiduYyConfig;
 import com.lcdt.traffic.dao.WaybillMapper;
 import com.lcdt.traffic.model.Waybill;
+import com.lcdt.traffic.service.LocationService;
 import com.lcdt.traffic.service.OwnDriverService;
 import com.lcdt.traffic.util.BalanceCheckBo;
 import com.lcdt.traffic.util.GprsLocationBo;
@@ -58,6 +59,9 @@ public class LocationServiceApi {
 
     @Autowired
     private WaybillMapper waybillMapper;
+
+    @Autowired
+    private LocationService locationService;
 
     @ApiOperation(value = "定位激活回调地址", notes = "用来接收基站定位第三方发送的回调信息（无任何权限控制）")
     @GetMapping("/callback")
@@ -216,72 +220,7 @@ public class LocationServiceApi {
     @PreAuthorize("hasRole('ROLE_SYS_ADMIN') or hasAuthority('lbs_location')")
     public JSONObject queryLocation(String mobile) {
         logger.debug("mobile:" + mobile);
-        JSONObject jsonObject = new JSONObject();
-        Long companyId = SecurityInfoGetter.getCompanyId(); //  获取companyId
-        if (!balanceCheckBo.check(companyId)) {
-            jsonObject.put("code", -1);
-            jsonObject.put("message", "余额不足！请充值！");
-            return jsonObject;
-        }
-        /**
-         * 先通过定位状态查询改手机号是否已激活，未激活提示需要开通才能定位，数据库状态变成0未开通
-         * 如果用户已激活则开始定位并把定位信息同步到数据库
-         * */
-        JSONObject openStatus = GprsLocationBo.getInstance().authStatus(mobile);
-        int resid = openStatus.getIntValue("resid");
-        if (resid == 1) {   //白名单且已开通定位，更新本地数据库定位状态为2
-            JSONObject result = GprsLocationBo.getInstance().queryLocation(mobile);
-            int resid1 = result.getIntValue("resid");
-            if (resid1 == 0) {   //已激活
-                balanceCheckBo.deductionGms(companyId); //查询正常扣费
-                Driver driver = new Driver();
-                driver.setDriverPhone(mobile);
-                driver.setCurrentLocation(result.getString("location"));
-                driver.setShortCurrentLocation(result.getString("street"));
-                //增加经纬度内容
-                driver.setLongitude(result.getString("lng"));
-                driver.setLatitude(result.getString("lat"));
-                driverService.updateLocation(driver);
-                //司机信息更新后上传鹰眼信息
-                MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
-                map.add("entity_name", mobile);
-                map.add("latitude", driver.getLatitude());
-                map.add("longitude", driver.getLongitude());
-                //上传鹰眼信息
-                new Thread(() -> upPoint(map)).start();
-
-//                upPoint(map);
-
-                jsonObject.put("code", resid1);
-                jsonObject.put("location", result.getString("location"));
-                jsonObject.put("locationTime", DateUtility.getCurrDatetime());
-                jsonObject.put("message", "查询成功");
-            } else if (resid1 == -80) {    //	余额不足,请充值:请联系客服
-                jsonObject.put("code", resid1);
-                jsonObject.put("message", "余额不足,请充值:请联系客服");
-            } else if (resid1 == -130) {    //用户可能关机
-                jsonObject.put("code", resid1);
-                jsonObject.put("message", "用户可能关机");
-            } else {      //对于移动手机，定位失败时运营商返回的结果
-                jsonObject.put("code", resid1);
-                jsonObject.put("message", "定位失败，请联系客服！");
-            }
-        } else if (resid == 0) {    //未激活
-            Driver driver = new Driver();
-            driver.setDriverPhone(mobile);
-            driver.setGpsStatus(new Short("1"));
-            driverService.modGpsStatus(driver);
-            jsonObject.put("code", -1);
-            jsonObject.put("message", "需要先开通定位服务才能定位");
-        } else if (resid == -1) {    //手机号不存在
-            jsonObject.put("code", resid);
-            jsonObject.put("message", "查找不到该手机");
-        } else {      //resid = -99  黑名单用户
-            jsonObject.put("code", resid);
-            jsonObject.put("message", "黑名单用户");
-        }
-
-        return jsonObject;
+        return locationService.queryLocation(mobile);
     }
 
     @ApiOperation(value = "列表获取位置信息", notes = "根据手机号获取位置(基站定位),多个手机号需要用逗号分隔")
@@ -322,22 +261,7 @@ public class LocationServiceApi {
         return restTemplate;
     }
 
-    public String upPoint(MultiValueMap map) {
-        RestTemplate restTemplate = getInstanceByCharset("utf-8");
-        map.add("ak", baiduYyConfig.getAk());
-        map.add("service_id", baiduYyConfig.getAk());
-        map.add("coord_type_input", "bd09ll");
-        map.add("loc_time", (System.currentTimeMillis() / 1000)+"");
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        HttpEntity entity = new HttpEntity(map, headers);
-        ResponseEntity<String> response = restTemplate.exchange("http://yingyan.baidu.com/api/v3/track/addpoint", HttpMethod.POST, entity, String.class);
-        String body = response.getBody();
-        System.out.println("body=="+body);
-        return  body;
-
-    }
     @ApiOperation(value = "获取轨迹", notes = "根据运单id获取轨迹")
     @GetMapping("/getTrack")
     public String getTrack(@RequestParam Long waybillId) {
