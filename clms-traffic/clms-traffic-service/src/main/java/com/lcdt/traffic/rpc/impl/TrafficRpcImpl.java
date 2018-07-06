3ba89639d5f47eda4396c3c5a696a2f308947973
yangbinq <ybq_9861@163.com>
2018-07-06 12:05:39 +0800

    'plan'

--- clms-traffic/clms-traffic-service/src/main/java/com/lcdt/traffic/rpc/impl/TrafficRpcImpl.java
+++ clms-traffic/clms-traffic-service/src/main/java/com/lcdt/traffic/rpc/impl/TrafficRpcImpl.java
@@ -175,8 +175,7 @@ public class TrafficRpcImpl implements TrafficRpc {
         vo.setIsDeleted((short) 0);
         vo.setCreateDate(dt);
         vo.setTransportWay((short) 1);
-        vo.setDistributionWay((short)4);
-        vo.setGroupName();
+        vo.setDistributionWay(4+"");
         waybillPlanMapper.insert(vo); //生成计划
         WaybillPlan vo2 = waybillPlanMapper.selectByWaybillPlanId(vo.getWaybillPlanId());
         List<PlanDetail> planDetailList = waybillParamsDto.getPlanDetailList();
