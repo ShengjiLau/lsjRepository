6334969330c39d4b4210572a6d421b805043a408
mawei <mawei@lichendt.com>
2018-07-10 18:55:25 +0800

    完善发布脚本

--- scripts/publish-user-service.sh
+++ scripts/publish-user-service.sh
@@ -6,9 +6,8 @@ docker login --username=hi35700248@aliyun.com -p A1111777  registry.cn-hangzhou.
 
 
 push_to_aliregistry(){
+cd $basepath
 
-cd ..
-basepath=$(cd `dirname $0`; pwd)
 echo build $servicename $basepath/$buildpath
 docker build -t $registry_url$servicename $basepath/$buildpath
 imageid=$(docker images | grep $registry_url$servicename | awk 'NR==1{print $3}')
@@ -19,6 +18,7 @@ docker push $registry_url$servicename:1.0
 
 
 findprojectpath(){
+    cd $scriptpath
     while IFS='' read -r line || [[ -n "$line" ]]; do
         str=(${line//:/ })
         if [[ $str == $1* ]];then
@@ -34,9 +34,10 @@ findprojectpath(){
 
 
 maven_build(){
+
+cd $basepath
 mvn clean install package -Dmaven.test.skip=true
 STATUS=$?
-
 if [[ $STATUS -eq 0 ]] ; then
 echo "编译成功"
 say "编译成功"
@@ -46,24 +47,22 @@ echo "编译失败"
 say "编译失败"
 fi
 }
-
+cd ..
+basepath=$(cd `dirname $0`; pwd)
+scriptpath=$basepath/scripts
 while [ "$1" ]
+do
 if [ "$1" = "-t" ];then
 maven_build
 else
 servicename=$1
-
-
-switch $1
-
-if [ $1 == t ];
-then
-  maven_build
 fi
+shift 1
+done
 
 docker_login
-
-findprojectpath $1
+echo $servicename
+findprojectpath $servicename
 push_to_aliregistry
 
 
