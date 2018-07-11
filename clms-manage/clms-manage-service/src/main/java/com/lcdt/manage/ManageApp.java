b43699a6e8132a18ab6f9d99f1a82ec8b4ce25df
xuerongrong <xuerongrong@lichendt.com>
2018-07-11 15:58:18 +0800

    增加管理后台服务模块

---
+++ clms-manage/clms-manage-service/src/main/java/com/lcdt/manage/ManageApp.java
@@ -0,0 +1,14 @@
+package com.lcdt.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Administrator on 2018/7/11.
+ */
+@SpringBootApplication
+public class ManageApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ManageApp.class, args);
+    }
+}
