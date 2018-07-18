cb78d2c4d43e45183c3c7f825cdfdb36c02bd2fe
zhaoshanliang <zhaoshanliang@lichendt.com>
2018-07-18 22:24:41 +0800

    网站页面

--- clms-manage/clms-manage-service/src/main/java/com/lcdt/manage/web.controller/FrontPageController.java
+++ clms-manage/clms-manage-service/src/main/java/com/lcdt/manage/web.controller/FrontPageController.java
@@ -17,7 +17,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by xrr on 2018/7/11.
@@ -70,7 +73,7 @@ public class FrontPageController {
         ModelAndView view = new ModelAndView(NEWS_LIST_PAGE);
         //view.addObject("info","这是咨询" );
         NoticeListParamsDto params = new NoticeListParamsDto();
-        params.setPageSize(10);
+        params.setPageSize(5);
 
         if (start != null && start > 0) {
             params.setPageNo(start);
@@ -84,6 +87,49 @@ public class FrontPageController {
             Page<NoticeListDto> page = noticeService.findTopNoticesByPage(params);
             List<NoticeListDto> notices = page.getRecords();
             view.addObject("notices", notices);
+
+            List<Map> pageNums = new ArrayList<Map>();
+            //分页栏的起止页码
+            int minPage=1,maxPage=1;
+            if(page.getCurrent()<=3){
+                minPage=1;
+                maxPage=Math.min(page.getPages(),5);
+            }
+            else if(page.getCurrent()>page.getPages()-2){
+                minPage = Math.max(page.getPages()-4,1);
+                maxPage = page.getPages();
+            }
+            else{
+                minPage = page.getCurrent()-2;
+                maxPage = page.getCurrent()+2;
+            }
+            //因页面的未知原因，数组倒着拍才能正确输出
+            Map next = new HashMap<>();
+            next.put("num",Math.min(page.getCurrent()+1,page.getPages()));
+            next.put("isPrevious",false);
+            next.put("isNext",true);
+            next.put("isCurrent",false);
+            pageNums.add(next);
+
+            for(int i=maxPage;i>=minPage;i--){
+                Map m = new HashMap<>();
+                m.put("num",i);
+                m.put("isCurrent",(page.getCurrent()==i));
+                m.put("isPrevious",false);
+                m.put("isNext",false);
+                pageNums.add(m);
+            }
+
+            Map previous = new HashMap<>();
+            previous.put("isPrevious",true);
+            previous.put("num",Math.max(page.getCurrent()-1,1));
+            previous.put("isNext",false);
+            previous.put("isCurrent",false);
+            pageNums.add(previous);
+
+
+            view.addObject("pageNums",pageNums);
+
         }
         return view;
     }
