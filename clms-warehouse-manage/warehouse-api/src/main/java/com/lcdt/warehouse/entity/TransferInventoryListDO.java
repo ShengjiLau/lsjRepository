03c608bfbb7568ab7c1bc132d5fb7280f58907fb
ShengjiLau <lsj616637@163.com>
2018-08-03 18:17:27 +0800

    。

--- clms-warehouse-manage/warehouse-api/src/main/java/com/lcdt/warehouse/entity/TransferInventoryListDO.java
+++ clms-warehouse-manage/warehouse-api/src/main/java/com/lcdt/warehouse/entity/TransferInventoryListDO.java
@@ -25,26 +25,10 @@ public class TransferInventoryListDO implements Serializable {
     private String warehouseName;
 	@ApiModelProperty("备注")
     private String remark;
-	@ApiModelProperty("附件1名称")
-    private String attachment1Name;
-	@ApiModelProperty("附件1")
-    private String attachment1;
-	@ApiModelProperty("附件2名称")
-    private String attachment2Name;
-	@ApiModelProperty("附件2")
-    private String attachment2;
-	@ApiModelProperty("附件3名称")
-    private String attachment3Name;
-	@ApiModelProperty("附件3")
-    private String attachment3;
-	@ApiModelProperty("附件4名称")
-    private String attachment4Name;
-	@ApiModelProperty("附件4")
-    private String attachment4;
-	@ApiModelProperty("附件5名称")
-    private String attachment5Name;
-	@ApiModelProperty("附件5")
-    private String attachment5;
+	@ApiModelProperty("附件名称")
+    private String attachmentName;
+	@ApiModelProperty("附件")
+    private String attachment;
 	@ApiModelProperty("创建时间")
     private Date gmtCreate;
 	@ApiModelProperty("修改时间")
@@ -55,10 +39,12 @@ public class TransferInventoryListDO implements Serializable {
     private Long createUserId;
 	@ApiModelProperty("所属公司id")
     private Long companyId;
-	@ApiModelProperty("转换单状态")
+	@ApiModelProperty("转换单状态：0-新建；1-完成；2-取消")
     private Byte listStatus;
 	@ApiModelProperty("流水号")
     private String listSerialNo;
+	@ApiModelProperty("完成时间")
+	private Date gmtComplete;
     
     private static final long serialVersionUID = 1L;
 
@@ -126,85 +112,7 @@ public class TransferInventoryListDO implements Serializable {
         this.remark = remark == null ? null : remark.trim();
     }
 
-    public String getAttachment1Name() {
-        return attachment1Name;
-    }
-
-    public void setAttachment1Name(String attachment1Name) {
-        this.attachment1Name = attachment1Name == null ? null : attachment1Name.trim();
-    }
-
-    public String getAttachment1() {
-        return attachment1;
-    }
-
-    public void setAttachment1(String attachment1) {
-        this.attachment1 = attachment1 == null ? null : attachment1.trim();
-    }
-
-    public String getAttachment2Name() {
-        return attachment2Name;
-    }
-
-    public void setAttachment2Name(String attachment2Name) {
-        this.attachment2Name = attachment2Name == null ? null : attachment2Name.trim();
-    }
-
-    public String getAttachment2() {
-        return attachment2;
-    }
-
-    public void setAttachment2(String attachment2) {
-        this.attachment2 = attachment2 == null ? null : attachment2.trim();
-    }
-
-    public String getAttachment3Name() {
-        return attachment3Name;
-    }
-
-    public void setAttachment3Name(String attachment3Name) {
-        this.attachment3Name = attachment3Name == null ? null : attachment3Name.trim();
-    }
-
-    public String getAttachment3() {
-        return attachment3;
-    }
-
-    public void setAttachment3(String attachment3) {
-        this.attachment3 = attachment3 == null ? null : attachment3.trim();
-    }
-
-    public String getAttachment4Name() {
-        return attachment4Name;
-    }
-
-    public void setAttachment4Name(String attachment4Name) {
-        this.attachment4Name = attachment4Name == null ? null : attachment4Name.trim();
-    }
-
-    public String getAttachment4() {
-        return attachment4;
-    }
-
-    public void setAttachment4(String attachment4) {
-        this.attachment4 = attachment4 == null ? null : attachment4.trim();
-    }
-
-    public String getAttachment5Name() {
-        return attachment5Name;
-    }
-
-    public void setAttachment5Name(String attachment5Name) {
-        this.attachment5Name = attachment5Name == null ? null : attachment5Name.trim();
-    }
-
-    public String getAttachment5() {
-        return attachment5;
-    }
-
-    public void setAttachment5(String attachment5) {
-        this.attachment5 = attachment5 == null ? null : attachment5.trim();
-    }
+   
 
     public Date getGmtCreate() {
         return gmtCreate;
@@ -262,110 +170,167 @@ public class TransferInventoryListDO implements Serializable {
         this.listSerialNo = listSerialNo == null ? null : listSerialNo.trim();
     }
 
-    @Override
-    public boolean equals(Object that) {
-        if (this == that) {
-            return true;
-        }
-        if (that == null) {
-            return false;
-        }
-        if (getClass() != that.getClass()) {
-            return false;
-        }
-        TransferInventoryListDO other = (TransferInventoryListDO) that;
-        return (this.getTransfersId() == null ? other.getTransfersId() == null : this.getTransfersId().equals(other.getTransfersId()))
-            && (this.getGroupId() == null ? other.getGroupId() == null : this.getGroupId().equals(other.getGroupId()))
-            && (this.getGroupName() == null ? other.getGroupName() == null : this.getGroupName().equals(other.getGroupName()))
-            && (this.getCustomerId() == null ? other.getCustomerId() == null : this.getCustomerId().equals(other.getCustomerId()))
-            && (this.getCustomerName() == null ? other.getCustomerName() == null : this.getCustomerName().equals(other.getCustomerName()))
-            && (this.getWarehouseId() == null ? other.getWarehouseId() == null : this.getWarehouseId().equals(other.getWarehouseId()))
-            && (this.getWarehouseName() == null ? other.getWarehouseName() == null : this.getWarehouseName().equals(other.getWarehouseName()))
-            && (this.getRemark() == null ? other.getRemark() == null : this.getRemark().equals(other.getRemark()))
-            && (this.getAttachment1Name() == null ? other.getAttachment1Name() == null : this.getAttachment1Name().equals(other.getAttachment1Name()))
-            && (this.getAttachment1() == null ? other.getAttachment1() == null : this.getAttachment1().equals(other.getAttachment1()))
-            && (this.getAttachment2Name() == null ? other.getAttachment2Name() == null : this.getAttachment2Name().equals(other.getAttachment2Name()))
-            && (this.getAttachment2() == null ? other.getAttachment2() == null : this.getAttachment2().equals(other.getAttachment2()))
-            && (this.getAttachment3Name() == null ? other.getAttachment3Name() == null : this.getAttachment3Name().equals(other.getAttachment3Name()))
-            && (this.getAttachment3() == null ? other.getAttachment3() == null : this.getAttachment3().equals(other.getAttachment3()))
-            && (this.getAttachment4Name() == null ? other.getAttachment4Name() == null : this.getAttachment4Name().equals(other.getAttachment4Name()))
-            && (this.getAttachment4() == null ? other.getAttachment4() == null : this.getAttachment4().equals(other.getAttachment4()))
-            && (this.getAttachment5Name() == null ? other.getAttachment5Name() == null : this.getAttachment5Name().equals(other.getAttachment5Name()))
-            && (this.getAttachment5() == null ? other.getAttachment5() == null : this.getAttachment5().equals(other.getAttachment5()))
-            && (this.getGmtCreate() == null ? other.getGmtCreate() == null : this.getGmtCreate().equals(other.getGmtCreate()))
-            && (this.getGmtModified() == null ? other.getGmtModified() == null : this.getGmtModified().equals(other.getGmtModified()))
-            && (this.getCreateUser() == null ? other.getCreateUser() == null : this.getCreateUser().equals(other.getCreateUser()))
-            && (this.getCreateUserId() == null ? other.getCreateUserId() == null : this.getCreateUserId().equals(other.getCreateUserId()))
-            && (this.getCompanyId() == null ? other.getCompanyId() == null : this.getCompanyId().equals(other.getCompanyId()))
-            && (this.getListStatus() == null ? other.getListStatus() == null : this.getListStatus().equals(other.getListStatus()))
-            && (this.getListSerialNo() == null ? other.getListSerialNo() == null : this.getListSerialNo().equals(other.getListSerialNo()));
-    }
+	public Date getGmtComplete() {
+		return gmtComplete;
+	}
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((getTransfersId() == null) ? 0 : getTransfersId().hashCode());
-        result = prime * result + ((getGroupId() == null) ? 0 : getGroupId().hashCode());
-        result = prime * result + ((getGroupName() == null) ? 0 : getGroupName().hashCode());
-        result = prime * result + ((getCustomerId() == null) ? 0 : getCustomerId().hashCode());
-        result = prime * result + ((getCustomerName() == null) ? 0 : getCustomerName().hashCode());
-        result = prime * result + ((getWarehouseId() == null) ? 0 : getWarehouseId().hashCode());
-        result = prime * result + ((getWarehouseName() == null) ? 0 : getWarehouseName().hashCode());
-        result = prime * result + ((getRemark() == null) ? 0 : getRemark().hashCode());
-        result = prime * result + ((getAttachment1Name() == null) ? 0 : getAttachment1Name().hashCode());
-        result = prime * result + ((getAttachment1() == null) ? 0 : getAttachment1().hashCode());
-        result = prime * result + ((getAttachment2Name() == null) ? 0 : getAttachment2Name().hashCode());
-        result = prime * result + ((getAttachment2() == null) ? 0 : getAttachment2().hashCode());
-        result = prime * result + ((getAttachment3Name() == null) ? 0 : getAttachment3Name().hashCode());
-        result = prime * result + ((getAttachment3() == null) ? 0 : getAttachment3().hashCode());
-        result = prime * result + ((getAttachment4Name() == null) ? 0 : getAttachment4Name().hashCode());
-        result = prime * result + ((getAttachment4() == null) ? 0 : getAttachment4().hashCode());
-        result = prime * result + ((getAttachment5Name() == null) ? 0 : getAttachment5Name().hashCode());
-        result = prime * result + ((getAttachment5() == null) ? 0 : getAttachment5().hashCode());
-        result = prime * result + ((getGmtCreate() == null) ? 0 : getGmtCreate().hashCode());
-        result = prime * result + ((getGmtModified() == null) ? 0 : getGmtModified().hashCode());
-        result = prime * result + ((getCreateUser() == null) ? 0 : getCreateUser().hashCode());
-        result = prime * result + ((getCreateUserId() == null) ? 0 : getCreateUserId().hashCode());
-        result = prime * result + ((getCompanyId() == null) ? 0 : getCompanyId().hashCode());
-        result = prime * result + ((getListStatus() == null) ? 0 : getListStatus().hashCode());
-        result = prime * result + ((getListSerialNo() == null) ? 0 : getListSerialNo().hashCode());
-        return result;
-    }
+	public void setGmtComplete(Date gmtComplete) {
+		this.gmtComplete = gmtComplete;
+	}
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", transfersId=").append(transfersId);
-        sb.append(", groupId=").append(groupId);
-        sb.append(", groupName=").append(groupName);
-        sb.append(", customerId=").append(customerId);
-        sb.append(", customerName=").append(customerName);
-        sb.append(", warehouseId=").append(warehouseId);
-        sb.append(", warehouseName=").append(warehouseName);
-        sb.append(", remark=").append(remark);
-        sb.append(", attachment1Name=").append(attachment1Name);
-        sb.append(", attachment1=").append(attachment1);
-        sb.append(", attachment2Name=").append(attachment2Name);
-        sb.append(", attachment2=").append(attachment2);
-        sb.append(", attachment3Name=").append(attachment3Name);
-        sb.append(", attachment3=").append(attachment3);
-        sb.append(", attachment4Name=").append(attachment4Name);
-        sb.append(", attachment4=").append(attachment4);
-        sb.append(", attachment5Name=").append(attachment5Name);
-        sb.append(", attachment5=").append(attachment5);
-        sb.append(", gmtCreate=").append(gmtCreate);
-        sb.append(", gmtModified=").append(gmtModified);
-        sb.append(", createUser=").append(createUser);
-        sb.append(", createUserId=").append(createUserId);
-        sb.append(", companyId=").append(companyId);
-        sb.append(", listStatus=").append(listStatus);
-        sb.append(", listSerialNo=").append(listSerialNo);
-        sb.append(", serialVersionUID=").append(serialVersionUID);
-        sb.append("]");
-        return sb.toString();
-    }
+	public String getAttachmentName() {
+		return attachmentName;
+	}
+
+	public void setAttachmentName(String attachmentName) {
+		this.attachmentName = attachmentName;
+	}
+
+	public String getAttachment() {
+		return attachment;
+	}
+
+	public void setAttachment(String attachment) {
+		this.attachment = attachment;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((attachment == null) ? 0 : attachment.hashCode());
+		result = prime * result + ((attachmentName == null) ? 0 : attachmentName.hashCode());
+		result = prime * result + ((companyId == null) ? 0 : companyId.hashCode());
+		result = prime * result + ((createUser == null) ? 0 : createUser.hashCode());
+		result = prime * result + ((createUserId == null) ? 0 : createUserId.hashCode());
+		result = prime * result + ((customerId == null) ? 0 : customerId.hashCode());
+		result = prime * result + ((customerName == null) ? 0 : customerName.hashCode());
+		result = prime * result + ((gmtComplete == null) ? 0 : gmtComplete.hashCode());
+		result = prime * result + ((gmtCreate == null) ? 0 : gmtCreate.hashCode());
+		result = prime * result + ((gmtModified == null) ? 0 : gmtModified.hashCode());
+		result = prime * result + ((groupId == null) ? 0 : groupId.hashCode());
+		result = prime * result + ((groupName == null) ? 0 : groupName.hashCode());
+		result = prime * result + ((listSerialNo == null) ? 0 : listSerialNo.hashCode());
+		result = prime * result + ((listStatus == null) ? 0 : listStatus.hashCode());
+		result = prime * result + ((remark == null) ? 0 : remark.hashCode());
+		result = prime * result + ((transfersId == null) ? 0 : transfersId.hashCode());
+		result = prime * result + ((warehouseId == null) ? 0 : warehouseId.hashCode());
+		result = prime * result + ((warehouseName == null) ? 0 : warehouseName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TransferInventoryListDO other = (TransferInventoryListDO) obj;
+		if (attachment == null) {
+			if (other.attachment != null)
+				return false;
+		} else if (!attachment.equals(other.attachment))
+			return false;
+		if (attachmentName == null) {
+			if (other.attachmentName != null)
+				return false;
+		} else if (!attachmentName.equals(other.attachmentName))
+			return false;
+		if (companyId == null) {
+			if (other.companyId != null)
+				return false;
+		} else if (!companyId.equals(other.companyId))
+			return false;
+		if (createUser == null) {
+			if (other.createUser != null)
+				return false;
+		} else if (!createUser.equals(other.createUser))
+			return false;
+		if (createUserId == null) {
+			if (other.createUserId != null)
+				return false;
+		} else if (!createUserId.equals(other.createUserId))
+			return false;
+		if (customerId == null) {
+			if (other.customerId != null)
+				return false;
+		} else if (!customerId.equals(other.customerId))
+			return false;
+		if (customerName == null) {
+			if (other.customerName != null)
+				return false;
+		} else if (!customerName.equals(other.customerName))
+			return false;
+		if (gmtComplete == null) {
+			if (other.gmtComplete != null)
+				return false;
+		} else if (!gmtComplete.equals(other.gmtComplete))
+			return false;
+		if (gmtCreate == null) {
+			if (other.gmtCreate != null)
+				return false;
+		} else if (!gmtCreate.equals(other.gmtCreate))
+			return false;
+		if (gmtModified == null) {
+			if (other.gmtModified != null)
+				return false;
+		} else if (!gmtModified.equals(other.gmtModified))
+			return false;
+		if (groupId == null) {
+			if (other.groupId != null)
+				return false;
+		} else if (!groupId.equals(other.groupId))
+			return false;
+		if (groupName == null) {
+			if (other.groupName != null)
+				return false;
+		} else if (!groupName.equals(other.groupName))
+			return false;
+		if (listSerialNo == null) {
+			if (other.listSerialNo != null)
+				return false;
+		} else if (!listSerialNo.equals(other.listSerialNo))
+			return false;
+		if (listStatus == null) {
+			if (other.listStatus != null)
+				return false;
+		} else if (!listStatus.equals(other.listStatus))
+			return false;
+		if (remark == null) {
+			if (other.remark != null)
+				return false;
+		} else if (!remark.equals(other.remark))
+			return false;
+		if (transfersId == null) {
+			if (other.transfersId != null)
+				return false;
+		} else if (!transfersId.equals(other.transfersId))
+			return false;
+		if (warehouseId == null) {
+			if (other.warehouseId != null)
+				return false;
+		} else if (!warehouseId.equals(other.warehouseId))
+			return false;
+		if (warehouseName == null) {
+			if (other.warehouseName != null)
+				return false;
+		} else if (!warehouseName.equals(other.warehouseName))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "TransferInventoryListDO [transfersId=" + transfersId + ", groupId=" + groupId + ", groupName="
+				+ groupName + ", customerId=" + customerId + ", customerName=" + customerName + ", warehouseId="
+				+ warehouseId + ", warehouseName=" + warehouseName + ", remark=" + remark + ", attachmentName="
+				+ attachmentName + ", attachment=" + attachment + ", gmtCreate=" + gmtCreate + ", gmtModified="
+				+ gmtModified + ", createUser=" + createUser + ", createUserId=" + createUserId + ", companyId="
+				+ companyId + ", listStatus=" + listStatus + ", listSerialNo=" + listSerialNo + ", gmtComplete="
+				+ gmtComplete + "]";
+	}
+	
+	
 }
