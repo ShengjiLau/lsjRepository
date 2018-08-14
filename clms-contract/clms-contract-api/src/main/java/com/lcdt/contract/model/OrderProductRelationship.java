ae60323439b0a00d715d86ba550bb97adf605968
lyqishan <lyqishan@126.com>
2018-08-14 10:42:09 +0800

    商品关系表、model、dao、xml

---
+++ clms-contract/clms-contract-api/src/main/java/com/lcdt/contract/model/OrderProductRelationship.java
@@ -0,0 +1,85 @@
+package com.lcdt.contract.model;
+
+import java.util.Date;
+
+public class OrderProductRelationship {
+    private Long relationshipId;
+
+    private Long opId;
+
+    private Long goodsId;
+
+    private Date createDate;
+
+    private String createName;
+
+    private Date updateDate;
+
+    private String updateName;
+
+    private Long companyId;
+
+    public Long getRelationshipId() {
+        return relationshipId;
+    }
+
+    public void setRelationshipId(Long relationshipId) {
+        this.relationshipId = relationshipId;
+    }
+
+    public Long getOpId() {
+        return opId;
+    }
+
+    public void setOpId(Long opId) {
+        this.opId = opId;
+    }
+
+    public Long getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(Long goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getCreateName() {
+        return createName;
+    }
+
+    public void setCreateName(String createName) {
+        this.createName = createName == null ? null : createName.trim();
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public String getUpdateName() {
+        return updateName;
+    }
+
+    public void setUpdateName(String updateName) {
+        this.updateName = updateName == null ? null : updateName.trim();
+    }
+
+    public Long getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Long companyId) {
+        this.companyId = companyId;
+    }
+}
