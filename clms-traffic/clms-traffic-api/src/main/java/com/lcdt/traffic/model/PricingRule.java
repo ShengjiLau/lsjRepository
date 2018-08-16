77a2e1d94726b7f42f69aaa08bba83e05174b8b2
liuhu <liuhu@lichendt.com>
2018-08-16 09:47:28 +0800

    计价规则实体及mapper生成

---
+++ clms-traffic/clms-traffic-api/src/main/java/com/lcdt/traffic/model/PricingRule.java
@@ -0,0 +1,95 @@
+package com.lcdt.traffic.model;
+
+import java.util.Date;
+
+public class PricingRule {
+    private Long prId;
+
+    private String ruleName;
+
+    private Short feeType;
+
+    private Short ruleUnit;
+
+    private Long createId;
+
+    private String createName;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private Long companyId;
+
+    public Long getPrId() {
+        return prId;
+    }
+
+    public void setPrId(Long prId) {
+        this.prId = prId;
+    }
+
+    public String getRuleName() {
+        return ruleName;
+    }
+
+    public void setRuleName(String ruleName) {
+        this.ruleName = ruleName == null ? null : ruleName.trim();
+    }
+
+    public Short getFeeType() {
+        return feeType;
+    }
+
+    public void setFeeType(Short feeType) {
+        this.feeType = feeType;
+    }
+
+    public Short getRuleUnit() {
+        return ruleUnit;
+    }
+
+    public void setRuleUnit(Short ruleUnit) {
+        this.ruleUnit = ruleUnit;
+    }
+
+    public Long getCreateId() {
+        return createId;
+    }
+
+    public void setCreateId(Long createId) {
+        this.createId = createId;
+    }
+
+    public String getCreateName() {
+        return createName;
+    }
+
+    public void setCreateName(String createName) {
+        this.createName = createName == null ? null : createName.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Long getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Long companyId) {
+        this.companyId = companyId;
+    }
+}
