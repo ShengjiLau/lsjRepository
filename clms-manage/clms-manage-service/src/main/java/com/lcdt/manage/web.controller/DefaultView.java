59c265386c7318b24abd78afb5fac2fefa85e883
zhaoshanliang <zhaoshanliang@lichendt.com>
2018-08-16 11:06:07 +0800

    网站路径优化

--- clms-manage/clms-manage-service/src/main/java/com/lcdt/manage/web.controller/DefaultView.java
+++ clms-manage/clms-manage-service/src/main/java/com/lcdt/manage/web.controller/DefaultView.java
@@ -13,7 +13,7 @@ public class DefaultView extends WebMvcConfigurerAdapter {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("forward:/front/index.html");
+        registry.addViewController("/").setViewName("forward:/portal/index.html");
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
         super.addViewControllers(registry);
     }
